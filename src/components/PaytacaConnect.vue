debb46e99902d793157ced7cf6cf8b58c8b16e5a
hantyrram <rongrammer@hotmail.com>
2023-06-26 16:03:01 +0800

    set ui to busy when connecting paytaca

--- src/components/PaytacaConnect.vue
+++ src/components/PaytacaConnect.vue
@@ -1,18 +1,28 @@
 <template>
-  <div>
+  <div class="q-my-sm q-mx-sm">
     <q-btn
-      size="xl"
-      round
-      color="primary"
+      size="md"
+      color="accent"
       icon="img:images/paytaca-128x128.png"
       @click.stop="connected ? disconnect(): connect()"
-    >{{ connected ? 'Disconnect': 'Connect' }}</q-btn>
+      align="center"
+      stack
+    >
+    <div class="row">
+      <div class="col">
+        <!-- {{ connected ? 'Disconnect': 'Connect' }} -->
+          <q-icon v-if="connected" name="link_off" size="xs"></q-icon>
+          <q-icon v-else name="link" size="xs"></q-icon>
+      </div>
+    </div>
+    </q-btn>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { useUserStore } from 'stores/user';
+import { useUIStore } from 'stores/ui';
 import formatAddress from 'src/utils/formatAddress';
 
 export default defineComponent({
@@ -24,14 +34,19 @@ export default defineComponent({
   },
   setup(props){
     const user = useUserStore()
+    const ui = useUIStore()
     return {
-      user
+      user,
+      ui
     }
   },
   methods: {
     async connect(){
+      const ui = useUIStore()
+      ui.busy({text: 'Connecting Paytaca', type: 'info'})
       let paytacaConnection = await window.paytaca!.connect()
       if(!paytacaConnection.connected) {
+          ui.idle()
           return
       }
       if (!paytacaConnection.address.startsWith('bitcoincash')) {
@@ -39,6 +54,7 @@ export default defineComponent({
       } 
       this.user.connectedPaytacaAddress = formatAddress(paytacaConnection.address)
       this.connected = true
+      ui.idle()
     },
     async disconnect(){
       await window.paytaca!.disconnect()
