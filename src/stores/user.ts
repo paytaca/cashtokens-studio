1ca57e1ceb13651ff4e028465c86abd1f0a409f8
hantyrram <rongrammer@hotmail.com>
2023-06-28 16:19:09 +0800

    added bch balance state on user store

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -3,6 +3,7 @@ import { defineStore } from 'pinia';
 export const useUserStore = defineStore('user', {
   state: () => ({
     connectedPaytacaAddress: '',
+    connectedPaytacaWalletBchBalance: 0
   }),
   getters: {
     // doubleCount: (state) => state.counter * 2,
