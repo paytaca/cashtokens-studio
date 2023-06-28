d3e3ec1a27c8e34b97dc05d134e45cb25e3dd80c
hantyrram <rongrammer@hotmail.com>
2023-06-28 16:17:24 +0800

    Updated sidebar menu,

---
+++ src/components/SidebarMenu.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-tree
+      :nodes="menu"
+      node-key="href"
+      no-connectors
+      v-model:expanded="expanded"
+      v-model:selected="selected"
+      default-expand-all
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { useUserStore } from 'src/stores/user'
+import { ref } from 'vue'
+
+
+export default {
+  setup () {
+    const user = useUserStore()
+    const selected = ref(null as any)
+    const lastSelectedBeforeUnselect = ref(null as any)
+    const expanded = ref([] as any)
+    const balance = user.connectedPaytacaWalletBchBalance
+    return {
+      // expanded: ref([ 'Satisfied customers (with avatar)', 'Good food (with icon)' ]),
+      user,
+      expanded,
+      selected,
+      lastSelectedBeforeUnselect,
+      menu: [
+        {
+          label: 'Issuer',
+          href: '#Issuer',
+          // avatar: 'https://cdn.quasar.dev/img/boy-avatar.png',
+          icon: 'domain_add',
+          children: [
+            {
+              label: 'New Fungible Token',
+              href: '/ft/create',
+              icon: 'add',
+              // children: [
+              //   { label: 'Quality ingredients' },
+              //   { label: 'Good recipe' }
+              // ],
+            },
+            {
+              label: 'New Non-Fungible Token',
+              href: '/nft/create',
+              icon: 'add',
+            },
+            {
+              label: 'Manage Tokens',
+              href: '#',
+              icon: 'token',
+              children: [
+                {
+                  label: 'View Issued FTs',
+                  href: '/ft/browse',
+                  icon: 'token',
+                  // img: 'https://cdn.quasar.dev/img/logo_calendar_128px.png'
+                },
+                {
+                  label: 'View Issued NFTs',
+                  href: '/nft/browse',
+                  icon: 'token',
+                  // img: 'https://cdn.quasar.dev/img/logo_calendar_128px.png'
+                }
+                
+              ]
+            }
+          ]
+        },
+        {
+          label: 'Wallet Balance',
+          href: '#Balance',
+          // avatar: 'https://cdn.quasar.dev/img/boy-avatar.png',
+          icon: 'account_balance_wallet',
+          children: [
+            
+            {
+              label: 'Coins (FTs)',
+              // icon: 'token',
+              avatar: 'https://cdn-icons-png.flaticon.com/128/5171/5171287.png',
+              // children: [
+              //   { label: 'Quality ingredients' },
+              //   { label: 'Good recipe' }
+              // ]
+            },
+            {
+              label: 'Collectibles (NFTs)',
+              avatar: 'https://img.uxwing.com/wp-content/themes/uxwing/download/internet-network-technology/non-fungible-tokens-nft-icon.svg',
+              // icon: 'restaurant_menu',
+              children: [
+                { label: 'Quality ingredients' },
+                { label: 'Good recipe' }
+              ]
+            },
+            {
+              label: balance,
+              avatar: 'https://chipnet.imaginary.cash/img/logo/bch.svg',
+              // icon: 'room_service',
+              // disabled: true,
+              // children: [
+              //   { label: 'Prompt attention' },
+              //   { label: 'Professional waiter' }
+              // ]
+            },
+            // {
+            //   label: 'Pleasant surroundings (with icon)',
+            //   icon: 'photo',
+            //   children: [
+            //     {
+            //       label: 'Happy atmosphere (with image)',
+            //       img: 'https://cdn.quasar.dev/img/logo_calendar_128px.png'
+            //     },
+            //     { label: 'Good table presentation' },
+            //     { label: 'Pleasing decor' }
+            //   ]
+            // }
+          ]
+        }
+      ]
+    }
+  },
+  watch: {
+    /**
+     * @param currentlySelected The node key (value of href in this case)
+     * @param previouslySelected The node key (value of href in this case)
+     */
+    selected(currentlySelected: string, previouslySelected){
+      /** 
+       * Toggle Expand / Collapse of menu with children on select
+       */
+      if (currentlySelected !== null) {
+        this.lastSelectedBeforeUnselect = currentlySelected
+      }
+
+      if (currentlySelected && !currentlySelected.startsWith('#')) {
+        this.$router.push(currentlySelected)
+        return
+      }
+      
+      if (currentlySelected === null) {
+        if (previouslySelected === this.lastSelectedBeforeUnselect) {
+          let menuIndex = this.expanded.findIndex((e: string) => e == this.lastSelectedBeforeUnselect)
+          this.expanded.splice(menuIndex, 1)
+        }
+      } else {
+        let indexOfCurrentlySelected = this.expanded.findIndex((e: string) => e == currentlySelected)
+        if (indexOfCurrentlySelected === -1) {
+          this.expanded.push(currentlySelected)
+        } else {
+          this.expanded.splice(indexOfCurrentlySelected, 1)
+        }
+      }
+    },
+    expanded(expanded){
+      console.log(expanded)
+    }
+  }
+}
+</script>
