dfcbdf74137587ab5c611b554d214bdc4a79d1a3
hantyrram <rongrammer@hotmail.com>
2023-07-03 17:18:05 +0800

    defined new states in pinia

--- src/stores/ui.ts
+++ src/stores/ui.ts
@@ -1,23 +1,39 @@
 import { defineStore } from 'pinia';
 
+declare type UIMessage = {
+  text: string,
+  type: ''| 'error' | 'warning' | 'info' | 'success',
+  timeout?: number  
+}
+
 export const useUIStore = defineStore('ui', {
   state: () => ({
+    paytacaInstalled: false,
     isBusy: false,
-    message: { text: '', type: '' as '' | 'error' | 'warning' | 'info' | 'success'}
+    message: {} as UIMessage
   }),
   getters: {
     // doubleCount: (state) => state.counter * 2,
   },
   actions: {
-    busy({text, type}){
+    busy(msg: UIMessage){
       this.isBusy = true
-      this.message.text = text
-      this.message.type = type
+      this.message.text = msg.text
+      this.message.type = msg.type
     },
     idle(){
       this.isBusy = false
       this.message.text = ''
       this.message.type = ''
+    },
+    clearMessage(){
+      this.message.text = ''
+      this.message.type = ''
+      delete this.message.timeout 
+    },
+    setMessage(msg: UIMessage) {
+      console.log('MESSAGE', msg)
+      this.message = msg
     }
   },
 });
--- src/stores/user.ts
+++ src/stores/user.ts
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia';
 export const useUserStore = defineStore('user', {
   state: () => ({
     connectedPaytacaAddress: '',
-    connectedPaytacaWalletBchBalance: 0
+    connectedPaytacaWalletBchBalance: '0'
   }),
   getters: {
     // doubleCount: (state) => state.counter * 2,
