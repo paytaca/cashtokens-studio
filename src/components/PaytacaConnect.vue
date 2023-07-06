fa0f75ed3f5cd1e1d78d5c20d5fb406148fe6c7c
hantyrram <rongrammer@hotmail.com>
2023-07-06 16:24:15 +0800

    refactored, used compositions api

--- src/components/PaytacaConnect.vue
+++ src/components/PaytacaConnect.vue
@@ -1,20 +1,14 @@
 <template>
   <div class="q-my-sm q-mx-sm">
-    <q-btn
-      size="md"
-      color="accent"
-      icon="img:images/paytaca-128x128.png"
-      @click.stop="user.connectedPaytacaAddress ? disconnect(): connect()"
-      align="center"
-      stack
-    >
-    <div class="row">
-      <div class="col">
-        <!-- {{ connected ? 'Disconnect': 'Connect' }} -->
+    <q-btn size="md" color="accent" icon="img:images/paytaca-128x128.png"
+      @click.stop="user.connectedPaytacaAddress ? disconnect() : connect()" align="center" stack>
+      <div class="row">
+        <div class="col">
+          <!-- {{ connected ? 'Disconnect': 'Connect' }} -->
           <q-icon v-if="user.connectedPaytacaAddress" name="link_off" size="xs"></q-icon>
           <q-icon v-else name="link" size="xs"></q-icon>
+        </div>
       </div>
-    </div>
     </q-btn>
   </div>
 </template>
@@ -26,15 +20,15 @@ import { useUIStore } from 'stores/ui';
 import formatAddress from 'src/utils/formatAddress';
 import getWalletClass from 'src/utils/getWalletClass';
 
-defineOptions({name: 'PaytacaConnect'})
-
+defineOptions({ name: 'PaytacaConnect' })
 const user = useUserStore()
 const ui = useUIStore()
 const connected = ref(false)
 
-const connect = async() => {
-  ui.busy({text: 'Connecting Paytaca', type: 'info'})
-  let paytacaConnection = await window.paytaca!.connect()  
+const connect = async () => {
+  ui.busy({ text: 'Connecting Paytaca', type: 'info' })
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  let paytacaConnection = await window.paytaca!.connect()
   if (paytacaConnection.connected) {
     if (!paytacaConnection.address.startsWith('bitcoincash')) {
       // TODO error
@@ -46,15 +40,15 @@ const connect = async() => {
     connected.value = true
     const WalletClass = getWalletClass()
     const wallet = await WalletClass.watchOnly(user.connectedPaytacaAddress)
-
     user.connectedPaytacaWalletBchBalance = String(await wallet.getBalance('sat'))
-    console.log(user.connectedPaytacaWalletBchBalance)
+
   } else {
     ui.idle()
   }
 }
 
-const disconnect = async() => {
+const disconnect = async () => {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   await window.paytaca!.disconnect()
   user.connectedPaytacaAddress = ''
   connected.value = false
