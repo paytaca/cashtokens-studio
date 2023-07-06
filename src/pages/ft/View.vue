2d68d2822b76108a423e664e8a3cfb3aab4f6a28
hantyrram <rongrammer@hotmail.com>
2023-07-06 16:28:07 +0800

    cleaned up, implementing bcmr updater function of existing tokens WIP

--- src/pages/ft/View.vue
+++ src/pages/ft/View.vue
@@ -2,14 +2,7 @@
   <q-page class="q-pa-md q-ma-sm" style="min-height: 100vh">
     <div>
       <div class="text-h6 q-mb-md">Viewing FT</div>
-      <q-tabs
-        v-model="tab"
-        dense
-        class="text-grey"
-        active-color="primary"
-        indicator-color="primary"
-        align="justify"
-      >
+      <q-tabs v-model="tab" dense class="text-grey" active-color="primary" indicator-color="primary" align="justify">
         <q-tab name="token" label="Token Details" />
         <q-tab v-if="enableBcmrEditor" name="bcmr" label="BCMR" />
       </q-tabs>
@@ -21,14 +14,15 @@
           <div class="row q-my-lg">
             <div class="col">
               <div class="col">
-              <q-input :filled="true" dark:color="lime" v-model="token.tokenId" label="Token Id" disable></q-input>
-            </div>  
-            </div>  
+                <q-input :filled="true" dark:color="lime" v-model="token.tokenId" label="Token Id" disable></q-input>
+              </div>
+            </div>
           </div>
           <div class="row q-my-lg">
             <div class="col">
-              <q-input :filled="true" dark:color="lime" v-model="token.creatorAddress" label="Creator's address" disable></q-input>
-            </div>  
+              <q-input :filled="true" dark:color="lime" v-model="token.creatorAddress" label="Creator's address"
+                disable></q-input>
+            </div>
           </div>
           <div class="row q-my-lg">
             <div class="col q-gutter-sm">
@@ -46,7 +40,7 @@
             </div>
           </div>
 
-          <JsonEditor v-model="bcmr" :darkTheme="$q.dark.isActive"/>
+          <JsonEditor v-model="bcmr" :darkTheme="$q.dark.isActive" />
         </q-tab-panel>
       </q-tab-panels>
     </div>
@@ -62,7 +56,8 @@
             <div class="col">
               <div class="row">
                 <div class="col">
-                  <q-input color="lime" :filled="true" standout bottom-slots v-model="bcmrCreationOption.fetchURL" label="Enter BCMR URL" clearable></q-input>
+                  <q-input color="lime" :filled="true" standout bottom-slots v-model="bcmrCreationOption.fetchURL"
+                    label="Enter BCMR URL" clearable></q-input>
                 </div>
               </div>
               <div class="row justify-end">
@@ -84,38 +79,44 @@
 <script setup lang="ts">
 
 import { sha256, utf8ToBin, binToHex, } from '@bitauth/libauth'
-import { hexToBin, OpReturnData } from 'mainnet-js'
+import { UtxoI } from 'mainnet-js'
+// import { hexToBin, OpReturnData } from 'mainnet-js'
 import JsonEditor from 'vue3-ts-jsoneditor'
-import { ref, onMounted} from 'vue'
+import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 // import { UtxoI } from 'mainnet-js'
 import { SignatureTemplate } from 'cashscript';
 
-import { Registry as Bcmr} from 'src/interfaces/bcmr-v2.schema'
+import { Registry as Bcmr } from 'src/interfaces/bcmr-v2.schema'
 import getWalletClass from 'src/utils/getWalletClass'
 // import { useUserStore } from 'src/stores/user'
 import bcmrTemplate from 'src/resources/bcmr'
 import { useUIStore } from 'src/stores/ui'
 import createAuthChainGuardContract from 'src/utils/createAuthChainGuardContract'
 import toCashScript from 'src/utils/toCashScript'
+import AuthChainGuard from '../../classes/AuthChainGuard'
+import { useUserStore } from 'src/stores/user';
 
-defineOptions({name: 'ViewFt'})
-
+defineOptions({ name: 'ViewFt' })
+const WalletClass = getWalletClass()
 const route = useRoute()
-// const user = useUserStore()
+const user = useUserStore()
 const ui = useUIStore()
+
+
+const authChainGuard = ref<AuthChainGuard | null>(null)
 
 const token = ref<{
   tokenId: string,
   creatorAddress: string,
-  }>({
+}>({
   tokenId: '',
   creatorAddress: '',
 })
 
 const tab = ref('token')
 const bcmr = ref<Bcmr>(bcmrTemplate)
-const bcmrCreationOption = ref<{option: 'fetch' | 'create', fetchURL?:string }>({option: 'fetch', fetchURL:'https://example.com/.well-known/bitcoin-cash-metadata-registry.json'})
+const bcmrCreationOption = ref<{ option: 'fetch' | 'create', fetchURL?: string }>({ option: 'fetch', fetchURL: 'https://example.com/.well-known/bitcoin-cash-metadata-registry.json' })
 const enableBcmrEditor = ref(false)
 // const openBcmrCreatorWizard = ref(false)
 const openBcmrFetchOrCreateDialog = ref(false)
@@ -130,80 +131,36 @@ onMounted(async () => {
 
 // methods
 const publishBcmrUpdate = async () => {
-  ui.busy({text: 'Updating BCMR', type: 'info'})
+
+  ui.busy({ text: 'Updating BCMR', type: 'info' })
+
+  const creatorWallet = await WalletClass.watchOnly(user.connectedPaytacaAddress)
+  const creatorWalletPkh = creatorWallet.getPublicKeyHash(false)
+  authChainGuard.value = new AuthChainGuard(creatorWalletPkh, creatorWallet.network)
+  const authChainGuardWallet = await WalletClass.watchOnly(authChainGuard.value.createContract().getDepositAddress())
+  const inputs = await authChainGuardWallet.getAddressUtxos()
   let bcmrText = JSON.stringify(bcmr.value);
   let contentHash = sha256.hash(utf8ToBin(bcmrText))
-  const WalletClass = getWalletClass()
-  const creatorWallet = await WalletClass.watchOnly(token.value.creatorAddress!)
-  const creatorWalletPkh = creatorWallet.getPublicKeyHash(false)
-  const contract = createAuthChainGuardContract({
-    ownerPubKey: creatorWalletPkh,
-    network: creatorWallet.network,
-  })
 
-  const func = contract.getContractFunction("TransferOrUpdateOrBurn");
-  let contractUtxos = (await contract.getUtxos()).filter(val => val.token?.tokenId === token.value.tokenId).map(toCashScript);
-  let contractInput = contractUtxos[0];
-
-  const ownerUtxo = (await creatorWallet.getAddressUtxos()).filter(val => !val.token && val.satoshis > 2000).map(toCashScript);
-  const ownerInput = ownerUtxo[0]
-  if (!ownerUtxo) {
-    ui.setMessage({type: 'error', text: 'No suitable utxos found to fund transaction', timeout:10000})
-    return;
-  }
-
-  const sig = new SignatureTemplate(Uint8Array.from(Array(32)))
-  
-  let transaction
-  let minerFee = 1000
-  try{
-    transaction = func(Uint8Array.from(Array(33)), Uint8Array.from(Array(65)))
-        .from(contractInput)
-        // .fromP2PKH(ownerInput, sig)
-    // // .from(contractInput)
-    // // .fromP2PKH(ownerUtxo, sig)
-        .to([
-          // contract pass-by
-          {
-            to: contract.getTokenDepositAddress(),
-            amount: contractInput.satoshis,
-            token: contractInput.token,
-          },
-          ])
-          .withOpReturn([
-            "BCMR",
-            binToHex(contentHash), // sha256 of the contents from the uri below
-            bcmr.registryIdentity!.uris.registry.replace("https://", "")
-          ])
-          .to([{
-            to: token.value.creatorAddress,
-            amount: ownerUtxo.satoshis - BigInt(minerFee)
-          }])
-          .withoutChange().withoutTokenChange().withHardcodedFee(BigInt(minerFee))
-
-  } catch {
-    ui.setMessage({type: 'error', text: 'Failed to build withdraw transaction', timeout:10000})
-    return;
-  }
 }
 
 const fetchBcmr = async () => {
   try {
-    ui.busy({text: `Fetching BCMR from ${bcmrCreationOption.value.fetchURL}`, type:'info'})
-    const r = await fetch(bcmrCreationOption.value.fetchURL!)  
+    ui.busy({ text: `Fetching BCMR from ${bcmrCreationOption.value.fetchURL}`, type: 'info' })
+    const r = await fetch(bcmrCreationOption.value.fetchURL!)
     bcmr.value = await r.json()
     enableBcmrEditor.value = true
     openBcmrFetchOrCreateDialog.value = false
     ui.idle()
-    ui.setMessage({text: 'BCMR download success, check the BCMR Tab', type: 'success', timeout: 5})
+    ui.setMessage({ text: 'BCMR download success, check the BCMR Tab', type: 'success', timeout: 5 })
     tab.value = 'bcmr'
   } catch (error) {
     ui.idle()
-    ui.setMessage({text: 'Failed to fetch BCMR, make sure the URL is correct', type: 'error', timeout: 5})
+    ui.setMessage({ text: 'Failed to fetch BCMR, make sure the URL is correct', type: 'error', timeout: 5 })
     console.log(error)
     enableBcmrEditor.value = false
     openBcmrFetchOrCreateDialog.value = false
-  } 
+  }
 }
 
 const createBcmr = async () => {
