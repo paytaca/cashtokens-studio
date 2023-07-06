59a4f5e1c27289ab0783f7fe3a9516d64c76677d
hantyrram <rongrammer@hotmail.com>
2023-07-06 16:25:52 +0800

    check manageable tokens, cross reference owned tokens from each token's authchain's authhead (WIP)

--- src/pages/ft/Browse.vue
+++ src/pages/ft/Browse.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="row justify-around q-ma-md">
-    <q-card v-for="ft, i in createdFts" :key="i" class="col-auto" @click="$router.push(`/ft/view?tokenId=${ft.token!.tokenId}&creator=${user.connectedPaytacaAddress}`)">
+    <q-card v-for="ft, i in createdFts" :key="i" class="col-auto"
+      @click="$router.push(`/ft/view?tokenId=${ft.token!.tokenId}&creator=${user.connectedPaytacaAddress}`)">
       <div class="text-h6">Token {{ i }}</div>
-      <q-avatar >
+      <q-avatar>
         <img src="https://chipnet.imaginary.cash/img/logo/bch.svg">
       </q-avatar>
       <div>{{ ft.token?.tokenId }}</div>
@@ -16,11 +17,11 @@
 </template>
 <script setup lang="ts">
 
-import { ref, onMounted, watch} from 'vue'
+import { ref, onMounted, watch } from 'vue'
 // import { sha256, utf8ToBin } from '@bitauth/libauth';
 // import { hexToBin, OpReturnData } from 'mainnet-js'
 // import JsonEditor from 'vue3-ts-jsoneditor'
-import { UtxoI } from 'mainnet-js'
+import { UtxoI, BCMR } from 'mainnet-js'
 
 // import { Registry as BcmrRegistry} from 'src/interfaces/bcmr-v2.schema';
 import getWalletClass from 'src/utils/getWalletClass';
@@ -30,7 +31,7 @@ import { useUserStore } from 'src/stores/user';
 // import TokenBcmrBasicForm from 'components/TokenBcmrBasicForm.vue'
 import createAuthChainGuardContract from 'src/utils/createAuthChainGuardContract';
 
-defineOptions({name: 'BrowseFt'})
+defineOptions({ name: 'BrowseFt' })
 
 const user = useUserStore()
 const createdFts = ref([] as UtxoI[])
@@ -50,16 +51,62 @@ onMounted(async () => {
 })
 
 // methods
+
 const loadCreatedFts = async (creatorAddress: string) => {
   const WalletClass = getWalletClass()
   const creatorWallet = await WalletClass.watchOnly(creatorAddress)
   const creatorWalletPkh = creatorWallet.getPublicKeyHash(false)
-  const contract = createAuthChainGuardContract({ownerPubKey: creatorWalletPkh, network: creatorWallet.network})
-  console.log('CREATOR', creatorWallet.getDepositAddress())
-  const contractWallet = await WalletClass.watchOnly(contract.getDepositAddress())
-  const txs = (await contractWallet.getAddressUtxos()).filter((utxo: UtxoI) => Boolean(utxo.token) && utxo.token!.amount > 0 );
-  console.log(txs)
-  createdFts.value = txs
+
+  const authchainGuardContract = createAuthChainGuardContract({
+    ownerPubKey: creatorWalletPkh,
+    network: creatorWallet.network,
+  })
+
+  const autchainGuardWallet = await WalletClass.watchOnly(authchainGuardContract.getDepositAddress())
+
+  creatorWallet.getTokenDepositAddress()
+  console.log(creatorWallet.getTokenDepositAddress())
+  // console.log(await creatorWallet.getAllTokenBalances())
+  // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain, @typescript-eslint/no-non-null-assertion
+  const creatorFts = (await creatorWallet.getAddressUtxos()).filter((utxo: UtxoI) => Boolean(utxo.token) && utxo.token?.amount! > 0)
+  createdFts.value = creatorFts
+
+  const authchainIdentityOutputs = (await autchainGuardWallet.getAddressUtxos()).filter((utxo: UtxoI) => Boolean(!utxo.token))
+  console.log(authchainIdentityOutputs)
+
+  let creatorFtsTokenIdsSet = new Set(creatorFts.map((utxo: UtxoI) => utxo.token?.tokenId))
+  let authchainIdentityOutputsSet = new Set(authchainIdentityOutputs.map((utxo: UtxoI) => utxo.txid))
+  // cross reference our authchainIdentityOutputsSet (txids) from the authheads of the authchains of each of our tokens
+  console.log('AUTH CHAIN IDENTITY OUTPUT SET', authchainIdentityOutputs)
+
+  console.log('LOADING AUTCHAIN')
+
+  const manageableTokens = []
+
+  creatorFtsTokenIdsSet.forEach(async (tokenId) => {
+    console.log('LOADING AUTCHAIN')
+    const response = await fetch(
+      'https://gql.chaingraph.pat.mn/v1/graphql',
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({
+          operationName: null,
+          variables: {},
+          // eslint-disable-next-line quotes
+          query: `{transaction(where:{hash:{_eq:\"\\\\x${tokenId}\"},block_inclusions:{block:{accepted_by:{node:{name:{_ilike:\"%chipnet%\"}}}}}}){ hash authchains {authchain_length migrations{transaction{hash inputs(where:{outpoint_index:{_eq:\"0\"}}){outpoint_index}outputs(where:{transaction:{outputs:{nonfungible_token_commitment:{_neq:\"null\"}}}}){output_index nonfungible_token_commitment}}}}}}`
+        })
+      })
+    let authchain = await response.json()
+    if (authchain) {
+      authchain.data
+    }
+  })
+
+
+
 }
 
 </script>
