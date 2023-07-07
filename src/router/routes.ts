731ecb60217b47cd284973474c7480820d013419
hantyrram <rongrammer@hotmail.com>
2023-07-07 17:16:36 +0800

    properly defined routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,3 +1,4 @@
+import { defineAsyncComponent } from 'vue';
 import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
@@ -7,36 +8,28 @@ const routes: RouteRecordRaw[] = [
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
   {
-    path: '/ft/browse',
+    path: '/ft',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/ft/Browse.vue') }],
+    children: [
+      { path: 'browse', component: defineAsyncComponent(() => import('../pages/ft/Browse.vue')) },
+      { path: 'view', component: () => import('pages/ft/View.vue') },
+      { path: 'new', component: () => import('pages/ft/New.vue') }
+    ],
   },
   {
-    path: '/ft/view',
+    path: '/nft',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/ft/View.vue') }],
-  },
-  {
-    path: '/ft/create',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/ft/Create.vue') }],
-  },
-  {
-    path: '/ft/new',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/ft/New.vue') }],
-  },
-  {
-    path: '/nft/create',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/nft/Create.vue') }],
+    children: [
+      { path: 'new', component: () => import('pages/nft/New.vue') },
+    ],
   },
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
-  },
+  }
+
 ];
 
 export default routes;
