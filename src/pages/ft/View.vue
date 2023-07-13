6e06e6c803f9cef2b4c8ac375082ea4b879415b2
hantyrram <rongrammer@hotmail.com>
2023-07-13 21:07:59 +0800

    cleaned up, renamed handlers

--- src/pages/ft/View.vue
+++ src/pages/ft/View.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable @typescript-eslint/no-non-null-assertion -->
 <template>
   <q-page class="q-pa-md q-ma-sm" style="min-height: 100vh">
     <div>
@@ -26,9 +27,9 @@
           </div>
           <div class="row q-my-lg">
             <div class="col q-gutter-sm">
-              <q-btn color="primary" size="md" @click.stop="updateBcmr">Transfer Ownership</q-btn>
               <q-btn color="primary" size="md" @click.stop="openBcmrFetchOrCreateDialog = true">Update Bcmr</q-btn>
-              <q-btn color="primary" size="md" @click.stop="updateBcmr">Burn</q-btn>
+              <q-btn color="primary" size="md" @click.stop="authchainTransfer">Transfer Ownership</q-btn>
+              <q-btn color="primary" size="md" @click.stop="authchainBurn">Burn</q-btn>
             </div>
           </div>
         </q-tab-panel>
@@ -36,7 +37,7 @@
           <div class="row text-h5 q-mb-md">
             <div class="col">
               <div>BCMR</div>
-              <q-btn @click="publishBcmrUpdate">Publish Update</q-btn>
+              <q-btn @click="authchainPublish">Publish Update</q-btn>
             </div>
           </div>
 
@@ -65,8 +66,6 @@
                   <q-btn color="primary" size="sm" @click="fetchBcmr">Fetch Updated BCMR</q-btn>
                   <q-btn color="primary" size="sm" @click="createBcmr">Create New</q-btn>
                 </div>
-                <!-- <q-btn color="primary" size="xs">Fetch</q-btn>
-                <q-btn color="primary" size="xs">Create New</q-btn> -->
               </div>
             </div>
           </div>
@@ -81,23 +80,20 @@
 import JsonEditor from 'vue3-ts-jsoneditor'
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
-
-
 import { Registry as Bcmr } from 'src/interfaces/bcmr-v2.schema'
 import { useUserStore } from 'src/stores/user';
 import { useUIStore } from 'src/stores/ui'
 import getWalletClass from 'src/utils/getWalletClass'
 import bcmrTemplate from 'src/resources/bcmr'
-import AuthChainGuard from '../../classes/AuthChainGuard'
+import AuthChainGuard from 'src/classes/AuthChainGuard'
 
 defineOptions({ name: 'ViewFt' })
+
 const WalletClass = getWalletClass()
-const route = useRoute()
 const user = useUserStore()
+const route = useRoute()
 const ui = useUIStore()
-
-
-// const authChainGuard = ref<AuthChainGuard | null>(null)
+const authChainGuard = ref<AuthChainGuard | null>(null)
 
 const token = ref<{
   tokenId: string,
@@ -111,21 +107,30 @@ const tab = ref('token')
 const bcmr = ref<Bcmr>(bcmrTemplate)
 const bcmrCreationOption = ref<{ option: 'fetch' | 'create', fetchURL?: string }>({ option: 'fetch', fetchURL: 'https://example.com/.well-known/bitcoin-cash-metadata-registry.json' })
 const enableBcmrEditor = ref(false)
-// const openBcmrCreatorWizard = ref(false)
-const openBcmrFetchOrCreateDialog = ref(false)
-
+const openBcmrFetchOrCreateDialog = ref(false) // ?? this should be a wizard
 
 onMounted(async () => {
   const { creator, tokenId } = route.query
   token.value.creatorAddress = String(creator)
   token.value.tokenId = String(tokenId)
   bcmrCreationOption.value.fetchURL = 'https://example.com/.well-known/bitcoin-cash-metadata-registry.json'
+  await initAuthChainGuard()
 })
 
+/**
+ * Create an instance of AuthChainGuard
+ */
+const initAuthChainGuard = async () => {
+  if (!authChainGuard.value) {
+    const creatorWallet = await WalletClass.watchOnly(user.connectedPaytacaAddress)
+    authChainGuard.value = new AuthChainGuard(user.connectedPaytacaAddress, creatorWallet.getPublicKeyHash(false), creatorWallet.network)
+  }
+}
+
 // methods
-const publishBcmrUpdate = async () => {
+
+const authchainPublish = async () => {
   ui.busy({ text: 'Updating BCMR', type: 'info' })
-  console.log(bcmr.value)
   const creatorWallet = await WalletClass.watchOnly(user.connectedPaytacaAddress)
   const creatorWalletPkh = creatorWallet.getPublicKeyHash(false)
   const authChainGuard = new AuthChainGuard(user.connectedPaytacaAddress, creatorWalletPkh, creatorWallet.network)
@@ -135,8 +140,7 @@ const publishBcmrUpdate = async () => {
     bcmrUrl = bcmr.value.registryIdentity.uris?.registry || ''
   }
   try {
-    const tx = await authChainGuard.updateBcmr(bcmrRawString, bcmrUrl)
-
+    const tx = await authChainGuard.publish(bcmrRawString, bcmrUrl)
     if (tx) {
       ui.idle()
       ui.setMessage({ type: 'success', text: `Bcmr Update Success! tx: ${tx}`, timeout: 5000 })
@@ -145,8 +149,10 @@ const publishBcmrUpdate = async () => {
   } catch (error) {
     console.log(error)
   }
-
 }
+
+const authchainTransfer = async () => { console.log('TODO') }
+const authchainBurn = async () => { console.log('TODO') }
 
 const fetchBcmr = async () => {
   try {
