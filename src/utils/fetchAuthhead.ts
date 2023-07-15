af7c74c159b9e4917e259e59f7ff715a22f7c9f6
hantyrram <rongrammer@hotmail.com>
2023-07-15 20:24:35 +0800

    created authhead resolver function

---
+++ src/utils/fetchAuthhead.ts
@@ -0,0 +1,18 @@
+export default async (tokenId: string): Promise<Response> => {
+  return await fetch(
+  'https://gql.chaingraph.pat.mn/v1/graphql',
+  {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      operationName: null,
+      variables: {},
+      // eslint-disable-next-line quotes
+      /* chaingraph authhead query*/
+      // eslint-disable-next-line quotes
+      query: `{transaction(where:{hash:{_eq:\"\\\\x${tokenId}\"},node_validation_timeline:{node:{name:{_ilike:\"%chipnet%\"}}}}){hash authchains{authchain_length migrations(where:{transaction:{outputs:{locking_bytecode_pattern:{_like:\"6a04%\"}}}},order_by:{migration_index:desc}limit:1){transaction{hash inputs(where:{outpoint_index:{_eq:\"0\"}}){outpoint_index}outputs(where:{locking_bytecode_pattern:{_like:\"6a04%\"}}){output_index locking_bytecode}}}}}}`
+    })
+  })
+}
