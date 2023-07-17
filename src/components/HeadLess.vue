1bb1b3b326bc8239755c92e26dfb60a8d987bd48
hantyrram <rongrammer@hotmail.com>
2023-07-17 12:42:29 +0800

    instantiate user wallet when connected

--- src/components/HeadLess.vue
+++ src/components/HeadLess.vue
@@ -4,10 +4,11 @@
 
 <script setup lang="ts">
 
-import { onMounted } from 'vue';
+import { onMounted, watch } from 'vue';
 import detectPaytaca from 'src/utils/detectPaytaca';
 import formatAddress from 'src/utils/formatAddress';
 import useStore from 'src/composables/useStore'
+import getWalletClass from 'src/utils/getWalletClass';
 
 defineOptions({ name: 'HeadLess' })
 
@@ -21,8 +22,14 @@ onMounted(async () => {
       let connectedAddress = await window.paytaca.address('bch')
       connectedAddress = formatAddress(connectedAddress)
       user.connectedPaytacaAddress = connectedAddress
-
+      user.wallet = await getWalletClass().watchOnly(connectedAddress)
     }
+  }
+})
+
+watch(() => user.connectedPaytacaAddress, async (address) => {
+  if (address) {
+    user.wallet = await getWalletClass().watchOnly(address)
   }
 })
 
