9c352e005f1d238e34364ddcb186b262c9a682c7
hantyrram <rongrammer@hotmail.com>
2023-07-17 12:41:35 +0800

    fixed, connected wallet's utxo not loading on page reload, enabled bcmr editor after creating bcmr

--- src/pages/token/Create.vue
+++ src/pages/token/Create.vue
@@ -1,17 +1,20 @@
 <template>
   <q-page class="q-pa-sm" style="min-height: 100vh">
-    <div class="row justify-between items-end q-mb-lg q-px-sm">
-      <i class="col-6 text-h5">Create Token</i>
+    <div class="row justify-between items-end q-mb-sm q-mx-lg">
+      <span class="col-6 text-h5"><q-icon name="token" /> Create Token</span>
       <div v-if="registry" class="col-6 row justify-end items-center q-gutter-sm">
         <div class="text-weight-thin">{BCMR}</div>
         <q-btn type="a" :href="`data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(registry))}`"
           download="bitcoin-cash-metadata-registy.json" icon="download" size="xs" round></q-btn>
-        <q-btn icon="edit" size="xs" round></q-btn>
-        <q-btn icon="delete" size="xs" color="red" round></q-btn>
+        <q-btn icon="edit" size="xs" round @click="genesisOptions.currentView = 'bcmr-editor'"></q-btn>
+        <q-btn icon="delete" size="xs" color="red" round
+          @click="() => { registry = null; genesisOptions.publishIdentityOutput = false; genesisOptions.currentView = 'create-token' }"></q-btn>
       </div>
     </div>
+    <q-separator :dark="$q.dark.isActive" class="q-mx-lg q-mb-lg" />
 
-    <q-form v-if="!genesisOptions.displayRegistryCreateWizard" class="q-gutter-md q-mx-sm">
+    <q-form v-if="genesisOptions.currentView === 'create-token'" class="q-gutter-md q-mx-lg q-pa-sm">
+      <i class="row text-h6 q-mb-sm">Token Details</i>
       <q-input class="row" :filled="true" dark:color="lime" v-model="creator" label="Creator's address"
         aria-disabled="true" disable dense square />
       <div class="row">
@@ -26,8 +29,12 @@
         </q-select>
       </div>
       <q-input v-if="token.tokenType === 'fungible' || token.tokenType === 'hybrid'" class="row" :filled="true"
-        dark:color="lime" v-model="token.amount" min="1" max="9223372036854700000" label="Max Supply" aria-disabled="true"
-        dense square />
+        dark:color="lime" v-model="token.amount" min="1" max="9223372036854700000" label="Genesis Supply"
+        aria-disabled="true" dense square>
+        <template v-slot:prepend>
+          <q-icon name="pin"></q-icon>
+        </template>
+      </q-input>
       <q-select v-if="token.tokenType === 'nonfungible' || token.tokenType === 'hybrid'" class="col q-mb-sm"
         :filled="true" bottom-slots v-model="token.capability" :options="['minting', 'mutable', 'none']"
         label="Capability" dense square>
@@ -38,25 +45,32 @@
       <q-input v-if="token.tokenType === 'nonfungible' || token.tokenType === 'hybrid'" class="row q-mb-sm" :filled="true"
         dark:color="lime" v-model="token.commitment" type="text" label="Commitment" dense square>
       </q-input>
-      <div class="row justify-end">
+      <div v-if="token.tokenId" class="row justify-end">
         <q-checkbox :filled="true" dark:color="lime" v-model="genesisOptions.publishIdentityOutput"
           label="Publish Identity Output">
         </q-checkbox>
       </div>
       <div class="row justify-end">
-        <q-btn color="secondary" size="large" @click="submitTokenGenesisTransaction">Create Token</q-btn>
+        <q-btn color="secondary" size="large" @click="submitTokenGenesisTransaction" :disable="!token.tokenId">Create
+          Token</q-btn>
       </div>
     </q-form>
     <!-- Bcmr Create Form Wizard -->
-    <BcmrBasicFormWizard v-if="genesisOptions.displayRegistryCreateWizard" type="fungible"
-      :token-id-options="token.idOptions" :bcmr="registry" :authbase="token.tokenId"
-      @finish="(r) => { genesisOptions.displayRegistryCreateWizard = false; registry = r }" />
+    <div v-if="genesisOptions.currentView === 'bcmr-wizard'" class="q-mx-lg q-pa-sm">
+      <i class="row text-h6 q-mb-sm">
+        Create BCMR
+      </i>
+      <BcmrBasicFormWizard type="fungible" :token-id-options="token.idOptions" :bcmr="registry" :authbase="token.tokenId"
+        @finish="(r) => { genesisOptions.currentView = 'create-token'; registry = r }"
+        @cancel="() => genesisOptions.currentView = 'create-token'" />
+    </div>
+
     <!-- Fetch Or Create Dialog -->
     <q-dialog :model-value="genesisOptions.displayFetchOrCreateDialog" full-width>
       <q-card class="q-pa-xs">
         <q-toolbar>
           <q-icon name="token" size="md"></q-icon>
-          <q-toolbar-title><span class="text-weight-bold">Fetch</span> or Create?</q-toolbar-title>
+          <q-toolbar-title><span class="text-weight-bold">Fetch Registry</span> or Create?</q-toolbar-title>
           <q-btn flat round dense icon="close" v-close-popup />
         </q-toolbar>
         <q-input :filled="true" v-model="registryUrl" label="Registry URL"></q-input>
@@ -66,6 +80,15 @@
         </div>
       </q-card>
     </q-dialog>
+    <!-- Bcmr editor -->
+    <div v-if="genesisOptions.currentView === 'bcmr-editor'" class="q-mx-lg q-pa-sm">
+      <div class="row justify-between">
+        <i class="text-h6 q-mb-sm">Review BCMR</i>
+        <q-btn icon="close" flat rounded size="sm" @click="genesisOptions.currentView = 'create-token'"></q-btn>
+      </div>
+
+      <JsonEditor v-model="registry" :darkTheme="$q.dark.isActive" />
+    </div>
   </q-page>
 </template>
 <style scoped></style>
@@ -73,6 +96,7 @@
 <script setup lang="ts">
 import { onMounted, ref, computed, watch } from 'vue'
 import { useRoute } from 'vue-router';
+import JsonEditor from 'vue3-ts-jsoneditor'
 import { sha256, utf8ToBin, decodeTransaction } from '@bitauth/libauth'
 import { hexToBin, BCMR, OpReturnData, SendRequest, TokenSendRequest, UnitEnum, UtxoI } from 'mainnet-js'
 
@@ -82,6 +106,7 @@ import { Registry as BcmrRegistry } from 'src/interfaces'
 import useStore from 'src/composables/useStore'
 import BcmrBasicFormWizard from 'src/components/BcmrBasicFormWizard.vue'
 import { TokenType } from 'src/types'
+
 defineOptions({ name: 'submitTokenGenesisTransaction' })
 
 const route = useRoute()
@@ -107,12 +132,16 @@ const token = ref<{
 
 const genesisOptions = ref<{
   publishIdentityOutput: boolean,
+  currentView: 'create-token' | 'bcmr-wizard' | 'bcmr-editor',
   displayRegistryCreateWizard: boolean,
-  displayFetchOrCreateDialog: boolean
+  displayFetchOrCreateDialog: boolean,
+  displayBcmrEditor: boolean
 }>({
+  currentView: 'create-token',
   publishIdentityOutput: false,
   displayRegistryCreateWizard: false,
-  displayFetchOrCreateDialog: false
+  displayFetchOrCreateDialog: false,
+  displayBcmrEditor: false
 })
 
 const registry = ref<BcmrRegistry | null>(null)
@@ -135,19 +164,22 @@ watch(() => route.params.tokenType, (tokenType) => {
 
 watch(() => user.connectedPaytacaAddress, async (address) => {
   if (address) {
-    const txIds = (await user.wallet?.getAddressUtxos())?.filter((utxo: UtxoI) => !utxo.token && utxo.vout === 0)
+    const creatorWallet = await getWalletClass().watchOnly(address)
+    const txIds = (await creatorWallet.getAddressUtxos())?.filter((utxo: UtxoI) => !utxo.token && utxo.vout === 0)
     token.value.idOptions = txIds?.map((utxo: UtxoI) => utxo.txid).slice(0, 9)
   }
 })
+
 onMounted(async () => {
   // let b = hexToBin('0x6578616d706c652e636f6d2f2e77656c6c2d6b6e6f776e2f626974636f696e2d636173682d6d657461646174612d72656769737472792e6a736f6e')
   // console.log(hexToBin('0x6578616d706c652e636f6d2f2e77656c6c2d6b6e6f776e2f626974636f696e2d636173682d6d657461646174612d72656769737472792e6a736f6e'))
   // console.log(binToUtf8(b))
   token.value.tokenType = route.params?.tokenType as TokenType
-  console.log('user wallet', user.wallet)
-  const txIds = (await user.wallet?.getAddressUtxos())?.filter((utxo: UtxoI) => !utxo.token && utxo.vout === 0)
-  console.log(txIds)
-  token.value.idOptions = txIds?.map((utxo: UtxoI) => utxo.txid).slice(0, 9)
+  if (creator.value) {
+    const creatorWallet = await getWalletClass().watchOnly(creator.value)
+    const txIds = (await creatorWallet.getAddressUtxos())?.filter((utxo: UtxoI) => !utxo.token && utxo.vout === 0)
+    token.value.idOptions = txIds?.map((utxo: UtxoI) => utxo.txid).slice(0, 9)
+  }
 })
 
 // methods
@@ -157,8 +189,10 @@ const fetchRegistry = async () => {
     const r = await fetch(registryUrl.value)
     registry.value = await r.json()
     registryObtainedFrom.value = 'fetch'
+    genesisOptions.value.displayFetchOrCreateDialog = false
     // TODO:Check if this token.tokenId's identity is in the registry
     ui.setMessage({ text: 'Registry download success', type: 'success', timeout: 5 })
+
   } catch (error) {
     ui.setMessage({ text: 'Failed to fetch registry, make sure the URI is correct', type: 'error', timeout: 5 })
     console.log(error)
@@ -169,7 +203,7 @@ const fetchRegistry = async () => {
 
 const displayRegistryCreateWizard = async () => {
   genesisOptions.value.displayFetchOrCreateDialog = false
-  genesisOptions.value.displayRegistryCreateWizard = true
+  genesisOptions.value.currentView = 'bcmr-wizard'
 }
 
 const submitTokenGenesisTransaction = async () => {
