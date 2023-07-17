1c5ee0733bf7014c1e6330dc2339d6359e640ea5
hantyrram <rongrammer@hotmail.com>
2023-07-17 17:32:50 +0800

    implemented registry parsing and registry update in token details page (WIP)

--- src/pages/token/View.vue
+++ src/pages/token/View.vue
@@ -1,113 +1,83 @@
 <!-- eslint-disable @typescript-eslint/no-non-null-assertion -->
 <template>
-  <q-page class="justify-center q-mx-xs" style="min-height:100vh; max-width:100vw">
+  <q-page class="justify-center q-mx-lg q-gutter-sm" style="min-height:100vh; max-width:100vw">
     <div class="row items-center q-gutter-lg">
-      <div>
-        <q-skeleton v-if="!token.icon" type="QAvatar" size="8em"></q-skeleton>
+      <div class="col">
+        <q-skeleton v-if="!token.identity?.uris?.icon" type="QAvatar" size="8em"></q-skeleton>
         <q-avatar v-else class="col" size="8em">
-          <img :src="token.icon" alt="">
+          <img :src="token.identity?.uris?.icon" alt="">
         </q-avatar>
       </div>
-      <div>
-        <div>Token Name</div>
-        <div class="ellipsis-2-lines">{{ token.tokenId?.replace(token.tokenId.substring(15, 45), '...') }}</div>
-        <div class="ellipsis-2-lines">Managed by: {{
-          token.creatorAddress.replace(token.creatorAddress.substring(15, 35), '...') }}</div>
+      <div class="col">
+        <div>Token: {{ token.identity?.name || 'Unknown' }}</div>
+        <div>Symbol: {{ token.identity?.token?.symbol }}</div>
+        <div class="ellipsis-2-lines">Id: {{ token.id?.replace(token.id.substring(15, 45), '...') }}</div>
+        <div class="ellipsis-2-lines">Manager: {{
+          token.creator?.replace(token.creator.substring(15, 35), '...') }}
+        </div>
+        <div v-if="token.identity?.uris">
+          <div v-for="k, i in Object.keys(token.identity?.uris)" :key="i">
+            {{ k }}: {{ token.identity?.uris[k] }}
+          </div>
+        </div>
       </div>
     </div>
+    <div v-if="token.identity?.description">
+      <p>{{ token.identity.description }}</p>
+    </div>
+    <div class="row justify-end">
+      <q-btn icon="settings" size="md">
+        <q-menu>
+          <q-list>
+            <q-item clickable v-close-popup>Publish Registry Update</q-item>
+            <q-item clickable v-close-popup>Transfer Ownership</q-item>
+            <q-item clickable v-close-popup>Burn Identity</q-item>
+          </q-list>
+        </q-menu>
+      </q-btn>
+    </div>
     <q-separator></q-separator>
-    <div class="row q-my-xs justify-end">
-      <q-btn color="primary" size="md" @click.stop="openBcmrFetchOrCreateDialog = true">Update Bcmr</q-btn>
+    <div v-if="menu == 'authchain-publish'" class="row justify-center">
+      <div class="col-12 justify-start q-my-sm">
+        <i class="text-h6">Publish Registry</i>
+      </div>
+
+      <q-input :filled="true" v-model="registryUrl" type="url" :rules="[v => v.length > 7 || 'Invalid URL']"
+        label="Registry URL" class="col-12" dense square></q-input>
+      <div class="row col-12">
+        <q-btn color="primary" size="xs" class="q-mr-xs" outline @click="fetchRegistry">
+          Fetch Registry
+          <q-tooltip>Fetch an existing registry from the above URL</q-tooltip>
+        </q-btn>
+        <q-btn color="primary" size="xs" outline @click="() => { console.log('TODO') }">
+          Create New Registry
+          <q-tooltip>Create a new registry and publish the above URL </q-tooltip>
+        </q-btn>
+      </div>
+      <div class="row col-12 justify-end">
+        <q-btn size="lg" color="primary" :disable="!registry" @click="authchainPublishRegistry">Publish Update</q-btn>
+      </div>
+    </div>
+    <!-- <div class="row q-my-xs justify-end">
+      <q-btn icon="settings"></q-btn>
+      <q-btn color="primary" size="md" @click.stop="authchainPublishRegistry">Update Registry</q-btn>
       <q-btn color="primary" size="md" @click.stop="authchainTransfer">Transfer Ownership</q-btn>
       <q-btn color="primary" size="md" @click.stop="authchainBurn">Burn</q-btn>
-    </div>
-    <!-- <div>
-      <q-tabs v-model="tab" dense class="text-grey" active-color="primary" indicator-color="primary" align="justify">
-        <q-tab name="token" label="Token Details" />
-        <q-tab v-if="enableBcmrEditor" name="bcmr" label="BCMR" />
-      </q-tabs>
-
-      <q-separator />
-
-      <q-tab-panels v-model="tab" animated>
-        <q-tab-panel name="token">
-          <div class="row q-my-lg">
-            <div class="col">
-              <div class="col">
-                <q-input :filled="true" dark:color="lime" v-model="token.tokenId" label="Token Id" disable></q-input>
-              </div>
-            </div>
-          </div>
-          <div class="row q-my-lg">
-            <div class="col">
-              <q-input :filled="true" dark:color="lime" v-model="token.creatorAddress" label="Creator's address"
-                disable></q-input>
-            </div>
-          </div>
-          <div class="row q-my-lg">
-            <div class="col q-gutter-sm">
-              <q-btn color="primary" size="md" @click.stop="openBcmrFetchOrCreateDialog = true">Update Bcmr</q-btn>
-              <q-btn color="primary" size="md" @click.stop="authchainTransfer">Transfer Ownership</q-btn>
-              <q-btn color="primary" size="md" @click.stop="authchainBurn">Burn</q-btn>
-            </div>
-          </div>
-        </q-tab-panel>
-        <q-tab-panel name="bcmr">
-          <div class="row text-h5 q-mb-md">
-            <div class="col">
-              <div>BCMR</div>
-              <q-btn @click="authchainPublish">Publish Update</q-btn>
-            </div>
-          </div>
-
-          <JsonEditor v-model="bcmr" :darkTheme="$q.dark.isActive" />
-        </q-tab-panel>
-      </q-tab-panels>
-    </div>
-    <q-dialog v-model="openBcmrFetchOrCreateDialog" @hide="openBcmrFetchOrCreateDialog = false">
-      <q-card style="width: 80vw;">
-        <q-toolbar>
-          <q-icon name="token" size="md"></q-icon>
-          <q-toolbar-title><span class="text-weight-bold">Fetch</span> or Create?</q-toolbar-title>
-          <q-btn flat round dense icon="close" v-close-popup />
-        </q-toolbar>
-        <q-card-section>
-          <div class="row">
-            <div class="col">
-              <div class="row">
-                <div class="col">
-                  <q-input color="lime" :filled="true" standout bottom-slots v-model="bcmrCreationOption.fetchURL"
-                    label="Enter BCMR URL" clearable></q-input>
-                </div>
-              </div>
-              <div class="row justify-end">
-                <div class="col-12 text-right q-gutter-sm q-pt-xs">
-                  <q-btn color="primary" size="sm" @click="fetchBcmr">Fetch Updated BCMR</q-btn>
-                  <q-btn color="primary" size="sm" @click="createBcmr">Create New</q-btn>
-                </div>
-              </div>
-            </div>
-          </div>
-        </q-card-section>
-      </q-card>
-    </q-dialog> -->
+    </div> -->
   </q-page>
 </template>
 
 <script setup lang="ts">
 
-import JsonEditor from 'vue3-ts-jsoneditor'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { hexToBin } from 'mainnet-js'
 import { binToUtf8 } from '@bitauth/libauth'
-import { Registry as Bcmr } from 'src/interfaces/bcmr-v2.schema'
+import { Registry as Bcmr, IdentitySnapshot, URIs } from 'src/interfaces/bcmr-v2.schema'
 import useStore from 'src/composables/useStore'
 import getWalletClass from 'src/utils/getWalletClass'
-import bcmrTemplate from 'src/resources/bcmr'
 import AuthChainGuard from 'src/classes/AuthChainGuard'
 import fetchAuthhead from 'src/utils/fetchAuthhead'
-
 
 defineOptions({ name: 'ViewFt' })
 
@@ -116,47 +86,55 @@ const { user, ui } = useStore()
 const route = useRoute()
 const authChainGuard = ref<AuthChainGuard | null>(null)
 
-const token = ref<{
-  icon: string,
-  tokenId: string,
-  creatorAddress: string,
-}>({
-  icon: '',
-  tokenId: '',
-  creatorAddress: '',
-})
+const token = ref<{ id?: string, creator?: string, identity?: IdentitySnapshot | null }>({})
+const registry = ref<Bcmr | null>(null)
+const registryUrl = ref<string>('')
+const menu = ref<'authchain-publish' | 'authchain-transfer' | 'authchain-burn'>('authchain-publish')
 
-const tab = ref('token')
-const bcmr = ref<Bcmr>(bcmrTemplate)
-const bcmrCreationOption = ref<{ option: 'fetch' | 'create', fetchURL?: string }>({ option: 'fetch', fetchURL: 'https://example.com/.well-known/bitcoin-cash-metadata-registry.json' })
-const enableBcmrEditor = ref(false)
-const openBcmrFetchOrCreateDialog = ref(false) // ?? this should be a wizard
+watch(() => registry.value, (r) => {
+  if (r) {
+    // parse registry and populate page
+    console.log('REGISTRY', r)
+    let identityHistory
+    if (token.value?.id && r.identities) {
+      identityHistory = r.identities[token.value.id]
+      if (identityHistory) {
+        let identityHistories = Object.keys(identityHistory)
+        token.value.identity = r.identities[token.value.id][identityHistories[identityHistories.length - 1]] // always get the last history
+      }
+    }
+  }
+})
 
 onMounted(async () => {
   const { creator, tokenId } = route.query
-  token.value.creatorAddress = String(creator)
-  token.value.tokenId = String(tokenId)
-  bcmrCreationOption.value.fetchURL = 'https://example.com/.well-known/bitcoin-cash-metadata-registry.json'
-  try {
-    let authheadResponse: Response = await fetchAuthhead(token.value.tokenId, user.walletNetworkType)
-    console.log(authheadResponse)
-    let authheadJson: { data: { transaction: [{ authchains: [{ migrations: [{ transaction: [{ hash: string, inputs: [any], outputs: [{ output_index: string, locking_bytecode: string }] }] }] }] }] } } = await authheadResponse.json()
-    let authhead = authheadJson.data?.transaction[0].authchains[0].migrations[0].transaction[0].outputs?.find(o => Number(o.output_index) == 1)
-    if (authhead) {
-      let tokenRegistryUri = binToUtf8(hexToBin(authhead.locking_bytecode).slice(73)) // 73 start index of URI's
-      console.log(tokenRegistryUri)
-      if (tokenRegistryUri) {
-        // dirty, assumes uri is valid, and only assumes https://
-        // TODO: improve, handle other URI protocol
-        let url = 'https://' + tokenRegistryUri.split(' ')[0]
-        // TODO: add check if url is valid or check pre-flight stats
-        let registry = await fetch(url)
-        registry = await registry.json()
-        // TODO: Update token page, based on this loaded registry, added error handled
-        console.log('REGISTRY', registry)
+  token.value.id = tokenId as string
+  token.value.creator = creator as string
 
+  try {
+    if (token.value.id) {
+      let authheadResponse: Response = await fetchAuthhead(token.value.id, user.walletNetworkType)
+      let authheadJson: { data: { transaction: [{ authchains: [{ migrations: [{ transaction: [{ hash: string, inputs: [any], outputs: [{ output_index: string, locking_bytecode: string }] }] }] }] }] } } = await authheadResponse.json()
+      let authhead = authheadJson.data?.transaction[0].authchains[0].migrations[0].transaction[0].outputs[0]
+      const op_returnAndBCMR = '\\x6a0442434d5220'
+      const registryHash = authhead.locking_bytecode.replace(op_returnAndBCMR, '').slice(0, 64)
+      let uris: string[] | string = authhead.locking_bytecode.replace(op_returnAndBCMR + registryHash, '')
+      uris = uris.substring(4, uris.length) // Remove the first 2 bytes, this is just the length of the uris
+      uris = uris.split('20') // split by spaces(20 = byte representation of space)
+      if (authhead) {
+        let tokenRegistryUri = binToUtf8(hexToBin(uris[0])) // 73 start index of URI's
+        if (tokenRegistryUri) {
+          // dirty, assumes uri is valid, and only assumes https://
+          // TODO: improve, handle other URI protocol
+          let url = 'https://' + tokenRegistryUri
+          // TODO: add check if url is valid or check pre-flight stats
+          let registryFetchResponse = await fetch(url)
+          registry.value = await registryFetchResponse.json()
+          // TODO: Update token page, based on this loaded registry, added error handled
+        }
       }
     }
+
   } catch (error) {
     console.log('Error fetching authhead from chaingraph', error)
   }
@@ -174,59 +152,42 @@ const initAuthChainGuard = async () => {
   }
 }
 
-
-
 // methods
-
-const authchainPublish = async () => {
-  ui.busy({ text: 'Updating BCMR', type: 'info' })
-  const creatorWallet = await WalletClass.watchOnly(user.connectedPaytacaAddress)
-  const creatorWalletPkh = creatorWallet.getPublicKeyHash(false)
-  const authChainGuard = new AuthChainGuard(user.connectedPaytacaAddress, creatorWalletPkh, creatorWallet.network)
-  const bcmrRawString = JSON.stringify(bcmr.value);
-  let bcmrUrl = 'https://'
-  if (typeof (bcmr.value.registryIdentity) !== 'string') {
-    bcmrUrl = bcmr.value.registryIdentity.uris?.registry || ''
+const authchainPublishRegistry = async () => {
+  console.log('Publishing registry')
+  authChainGuard.value
+  if (!registry.value) {
+    return ui.setMessage({ text: 'Invalid BCMR', type: 'error', timeout: 10 })
   }
   try {
-    const tx = await authChainGuard.publish(bcmrRawString, bcmrUrl)
-    if (tx) {
-      ui.idle()
-      ui.setMessage({ type: 'success', text: `Bcmr Update Success! tx: ${tx}`, timeout: 5000 })
-    }
-
+    ui.busy({ text: 'Publishing registry', type: 'info' })
+    const tx = await authChainGuard.value?.publish(JSON.stringify(registry.value), registryUrl.value)
+    ui.idle()
+    ui.setMessage({ text: 'Registry publication success, tx:' + tx, type: 'success', timeout: 5 })
   } catch (error) {
+    ui.idle()
     console.log(error)
+    ui.setMessage({ text: 'Error publishing registry', type: 'error', timeout: 10 })
   }
+
 }
 
 const authchainTransfer = async () => { console.log('TODO') }
 const authchainBurn = async () => { console.log('TODO') }
 
-const fetchBcmr = async () => {
+const fetchRegistry = async () => {
   try {
-    ui.busy({ text: `Fetching BCMR from ${bcmrCreationOption.value.fetchURL}`, type: 'info' })
-    const r = await fetch(bcmrCreationOption.value.fetchURL!)
-    bcmr.value = await r.json()
-    enableBcmrEditor.value = true
-    openBcmrFetchOrCreateDialog.value = false
+    ui.busy({ text: `Fetching registry from ${registryUrl.value}`, type: 'info' })
+    const r = await fetch(registryUrl.value)
+    registry.value = await r.json()
+    // TODO:Check if this token.tokenId's identity is in the registry
     ui.idle()
-    ui.setMessage({ text: 'BCMR download success, check the BCMR Tab', type: 'success', timeout: 5 })
-    tab.value = 'bcmr'
+    ui.setMessage({ text: 'Registry download success', type: 'success', timeout: 5 })
   } catch (error) {
     ui.idle()
-    ui.setMessage({ text: 'Failed to fetch BCMR, make sure the URL is correct', type: 'error', timeout: 5 })
+    ui.setMessage({ text: 'Failed to fetch registry, make sure the URI is correct', type: 'error', timeout: 5 })
     console.log(error)
-    enableBcmrEditor.value = false
-    openBcmrFetchOrCreateDialog.value = false
   }
-}
-
-const createBcmr = async () => {
-  enableBcmrEditor.value = true
-  bcmr.value = bcmrTemplate
-  tab.value = 'bcmr'
-  openBcmrFetchOrCreateDialog.value = false
 }
 </script>
 
