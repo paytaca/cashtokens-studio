5a543fce77397a77a32204165629b6b3d8b095fb
hantyrram <rongrammer@hotmail.com>
2023-07-18 14:09:00 +0800

    removed unused routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -8,28 +8,12 @@ const routes: RouteRecordRaw[] = [
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
   {
-    path: '/ft',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: 'browse', component: defineAsyncComponent(() => import('../pages/ft/Browse.vue')) },
-      { path: 'view', component: () => import('pages/ft/View.vue') },
-      { path: 'new', component: () => import('pages/ft/New.vue') }
-    ],
-  },
-  {
     path: '/token',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: 'browse/:tokenType?', component: defineAsyncComponent(() => import('pages/token/Browse.vue')) },
       { path: 'view', component: () => import('pages/token/View.vue') },
       { path: 'create/:tokenType?', component: () => import('pages/token/Create.vue') }
-    ],
-  },
-  {
-    path: '/nft',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: 'new', component: () => import('pages/nft/New.vue') },
     ],
   },
   // Always leave this as last one,
