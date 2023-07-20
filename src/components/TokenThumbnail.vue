6bff4748076d68f808be722b39c7411774bd9a93
hantyrram <rongrammer@hotmail.com>
2023-07-20 16:08:48 +0800

    added token thumbnail component

---
+++ src/components/TokenThumbnail.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-md row items-start">
+      <q-card flat bordered style="width: 250px">
+        <q-card-section>
+          <span class="text-caption">{{ name }}</span>
+        </q-card-section>
+        <q-separator />
+        <q-card-section>
+          <q-avatar>
+            <img :src="icon" />
+          </q-avatar>
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+
+defineOptions({ name: 'TokenThumbnail' })
+const props = defineProps(['icon', 'symbol', 'name'])
+</script>
---
+++ src/components/skeletons/TokenThumbnail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-md row items-start">
+      <q-card flat bordered style="width: 250px">
+        <q-card-section>
+          <q-skeleton class="text-caption" type="text" />
+        </q-card-section>
+        <q-separator />
+        <q-card-section>
+          <q-skeleton type="QAvatar" />
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
+</template>
