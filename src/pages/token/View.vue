2122d5872a17169768525590c53b94673449931b
hantyrram <rongrammer@hotmail.com>
2023-07-21 16:06:11 +0800

    cleaned up token page

--- src/pages/token/View.vue
+++ src/pages/token/View.vue
@@ -1,115 +1,119 @@
 <!-- eslint-disable @typescript-eslint/no-non-null-assertion -->
 <template>
-  <q-page class="justify-center q-mx-lg q-gutter-sm" style="min-height:100vh; max-width:100vw">
-    <div class="row inline items-center q-gutter-lg">
-      <div class="items-center justify-center q-px-xl">
-        <q-skeleton v-if="!token.identity?.uris?.icon" type="QAvatar" size="8em"></q-skeleton>
-        <q-avatar v-else size="8em">
-          <img :src="token.identity?.uris?.icon" alt="">
-        </q-avatar>
-      </div>
-      <div>
-        <div>
-          <q-skeleton v-if="loading" type="text" height="3em"></q-skeleton>
-          <span v-else>Token: {{ token.identity?.name || 'Unknown' }}</span>
-        </div>
-        <div>
-          <q-skeleton v-if="loading" type="text" height="3em"></q-skeleton>
-          <span v-else>Symbol: <q-badge outline color="orange" :label="token.identity?.token?.symbol" /></span>
-        </div>
-        <div>
-          <q-skeleton v-if="loading" type="text" height="3em"></q-skeleton>
-          <span v-else>Decimals: {{ token.identity?.token?.decimals }}</span>
-        </div>
-        <div>
-          <q-skeleton v-if="loading" type="text" height="3em"></q-skeleton>
-          <span v-else>
-            Category (Token Id): {{ token.id?.replace(token.id.substring(15, 45), '...') }}
-          </span>
-        </div>
-        <div class="ellipsis-2-lines">
-          Manager: {{
-            token.creator?.replace(token.creator.substring(15, 35), '...') }}
-        </div>
-        <div v-if="token.identity?.uris">
-          <div v-for="k, i in Object.keys(token.identity?.uris)" :key="i">
-            <div v-if="k === 'icon'">
-              Icon: <q-avatar size="sm">
-                <img :src="token.identity?.uris[k]" />
-              </q-avatar>
+  <q-page>
+    <div class="row justify-center">
+      <div class="col-xs-12 col-md-10 col-lg-8">
+        <div class="row inline items-center q-gutter-lg">
+          <div class="items-center justify-center q-px-xl">
+            <q-skeleton v-if="!token.identity?.uris?.icon" type="QAvatar" size="8em"></q-skeleton>
+            <q-avatar v-else size="8em">
+              <img :src="token.identity?.uris?.icon" alt="">
+            </q-avatar>
+          </div>
+          <div>
+            <div>
+              <q-skeleton v-if="loading" type="text" height="3em"></q-skeleton>
+              <span v-else>Token: {{ token.identity?.name || 'Unknown' }}</span>
             </div>
-            <span v-else>
-              {{ k }}: {{ token.identity?.uris[k] }}
-            </span>
+            <div>
+              <q-skeleton v-if="loading" type="text" height="3em"></q-skeleton>
+              <span v-else>Symbol: <q-badge outline color="orange" :label="token.identity?.token?.symbol" /></span>
+            </div>
+            <div>
+              <q-skeleton v-if="loading" type="text" height="3em"></q-skeleton>
+              <span v-else>Decimals: {{ token.identity?.token?.decimals }}</span>
+            </div>
+            <div>
+              <q-skeleton v-if="loading" type="text" height="3em"></q-skeleton>
+              <span v-else>
+                Category (Token Id): {{ token.id?.replace(token.id.substring(15, 45), '...') }}
+              </span>
+            </div>
+            <div class="ellipsis-2-lines">
+              Manager: {{
+                token.creator?.replace(token.creator.substring(15, 35), '...') }}
+            </div>
+            <div v-if="token.identity?.uris">
+              <div v-for="k, i in Object.keys(token.identity?.uris)" :key="i">
+                <div v-if="k === 'icon'">
+                  Icon: <q-avatar size="sm">
+                    <img :src="token.identity?.uris[k]" />
+                  </q-avatar>
+                </div>
+                <span v-else>
+                  {{ k }}: {{ token.identity?.uris[k] }}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div v-if="token.identity?.description">
+          <q-banner rounded dense>
+            <p>{{ token.identity.description }}</p>
+          </q-banner>
+        </div>
+        <div class="row justify-end">
+          <q-btn icon="settings" size="md">
+            <q-menu>
+              <q-list>
+                <q-item clickable v-close-popup @click="menu = 'authchain-publish'">Publish Registry Update</q-item>
+                <q-item clickable v-close-popup @click="menu = 'authchain-transfer'">Transfer Ownership</q-item>
+                <q-item clickable v-close-popup @click="menu = 'authchain-burn'">Burn Identity</q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
+        </div>
+        <q-separator></q-separator>
+        <div v-if="menu == 'authchain-publish'" class="row justify-center">
+          <div class="col-12 justify-start q-my-sm">
+            <i class="text-h6">Publish Registry</i>
+          </div>
+          <q-input :filled="true" v-model="registryUrl" type="url" :rules="[v => v.length > 7 || 'Invalid URL']"
+            label="Registry URL" class="col-12" dense square></q-input>
+          <div class="row col-12">
+            <q-btn color="primary" size="xs" class="q-mr-xs" outline @click="fetchRegistry">
+              Fetch Registry
+              <q-tooltip>Fetch an existing registry from the above URL</q-tooltip>
+            </q-btn>
+            <q-btn color="primary" size="xs" outline @click="() => { console.log('TODO') }">
+              Create New Registry
+              <q-tooltip>Create a new registry and publish the above URL </q-tooltip>
+            </q-btn>
+          </div>
+          <div class="row col-12 justify-end">
+            <q-btn size="lg" color="primary" :disable="!registry || !registryUrl" @click="authchainPublishRegistry">
+              Confirm Publish
+            </q-btn>
+          </div>
+        </div>
+        <div v-if="menu == 'authchain-transfer'" class="row justify-center">
+          <div class="col-12 justify-start q-my-sm">
+            <i class="text-h6">Transfer Authchain Ownership</i>
+          </div>
+          <q-input :filled="true" v-model="newOwnerAddress" type="url" :rules="[v => v.length > 49 || 'Invalid Address']"
+            label="New owner's address" class="col-12" dense square></q-input>
+          <div class="row col-12 justify-end">
+            <q-btn size="lg" color="primary" :disable="!newOwnerAddress" @click="authchainTransfer">
+              Confirm Transfer
+            </q-btn>
+          </div>
+        </div>
+        <div v-if="menu == 'authchain-burn'" class="row justify-center">
+          <div class="col-12 justify-start q-my-sm">
+            <i class="text-h6">Are you sure you want to burn this token's authchain?</i>
+          </div>
+          <div class="row col-12 justify-end q-gutter-sm">
+            <q-btn size="lg" color="negative" @click="() => { menu = '' }">
+              No
+            </q-btn>
+            <q-btn size="lg" color="secondary" @click="authchainBurn">
+              Yes
+            </q-btn>
           </div>
         </div>
       </div>
     </div>
-    <div v-if="token.identity?.description">
-      <q-banner rounded dense>
-        <p>{{ token.identity.description }}</p>
-      </q-banner>
-    </div>
-    <div class="row justify-end">
-      <q-btn icon="settings" size="md">
-        <q-menu>
-          <q-list>
-            <q-item clickable v-close-popup @click="menu = 'authchain-publish'">Publish Registry Update</q-item>
-            <q-item clickable v-close-popup @click="menu = 'authchain-transfer'">Transfer Ownership</q-item>
-            <q-item clickable v-close-popup @click="menu = 'authchain-burn'">Burn Identity</q-item>
-          </q-list>
-        </q-menu>
-      </q-btn>
-    </div>
-    <q-separator></q-separator>
-    <div v-if="menu == 'authchain-publish'" class="row justify-center">
-      <div class="col-12 justify-start q-my-sm">
-        <i class="text-h6">Publish Registry</i>
-      </div>
-      <q-input :filled="true" v-model="registryUrl" type="url" :rules="[v => v.length > 7 || 'Invalid URL']"
-        label="Registry URL" class="col-12" dense square></q-input>
-      <div class="row col-12">
-        <q-btn color="primary" size="xs" class="q-mr-xs" outline @click="fetchRegistry">
-          Fetch Registry
-          <q-tooltip>Fetch an existing registry from the above URL</q-tooltip>
-        </q-btn>
-        <q-btn color="primary" size="xs" outline @click="() => { console.log('TODO') }">
-          Create New Registry
-          <q-tooltip>Create a new registry and publish the above URL </q-tooltip>
-        </q-btn>
-      </div>
-      <div class="row col-12 justify-end">
-        <q-btn size="lg" color="primary" :disable="!registry || !registryUrl" @click="authchainPublishRegistry">
-          Confirm Publish
-        </q-btn>
-      </div>
-    </div>
-    <div v-if="menu == 'authchain-transfer'" class="row justify-center">
-      <div class="col-12 justify-start q-my-sm">
-        <i class="text-h6">Transfer Authchain Ownership</i>
-      </div>
-      <q-input :filled="true" v-model="newOwnerAddress" type="url" :rules="[v => v.length > 49 || 'Invalid Address']"
-        label="New owner's address" class="col-12" dense square></q-input>
-      <div class="row col-12 justify-end">
-        <q-btn size="lg" color="primary" :disable="!newOwnerAddress" @click="authchainTransfer">
-          Confirm Transfer
-        </q-btn>
-      </div>
-    </div>
-    <div v-if="menu == 'authchain-burn'" class="row justify-center">
-      <div class="col-12 justify-start q-my-sm">
-        <i class="text-h6">Are you sure you want to burn this token's authchain?</i>
-      </div>
 
-      <div class="row col-12 justify-end q-gutter-sm">
-        <q-btn size="lg" color="negative" @click="() => { menu = '' }">
-          No
-        </q-btn>
-        <q-btn size="lg" color="secondary" @click="authchainBurn">
-          Yes
-        </q-btn>
-      </div>
-    </div>
   </q-page>
 </template>
 
