d1168044d79dceb173312c544acd0d1ea1f28e2b
hantyrram <rongrammer@hotmail.com>
2023-07-21 15:46:05 +0800

    display token id on thumbnail

---
+++ src/components/TokenCreateThumbnail.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-btn color="primary col-sm-3 col-md-2 items-center" @click="router.push('/token/create')">
+    <q-icon name="add" size="lg"></q-icon><span>Create New Token</span>
+  </q-btn>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+defineOptions({ name: 'TokenCreateThumbnail' })
+defineProps(['path'])
+</script>
