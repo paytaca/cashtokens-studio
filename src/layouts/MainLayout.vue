c5d43f0832dd98105b9942ec0fad04e6741d721c
hantyrram <rongrammer@hotmail.com>
2023-07-24 16:10:54 +0800

    simplified sidebar for now

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,7 +2,7 @@
   <q-layout view="lHh Lpr lFf">
     <HeadLess />
     <q-header elevated>
-      <q-toolbar>
+      <q-toolbar class="bg-teal-10">
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
         <q-toolbar-title>
           Cashtokens Studio
@@ -11,31 +11,21 @@
         <paytaca-connect />
       </q-toolbar>
     </q-header>
-
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header class="q-mx-sm">
-          <div style="position: relative">
-            <svg class="svg" viewBox="0 0 100 100" width="150">
-              <defs>
-                <path id="circle" d="
-                      M 50, 50
-                      m -37, 0
-                      a 37,37 0 1,1 74,0
-                      a 37,37 0 1,1 -74,0" />
-              </defs>
-              <text font-size="12" style="transform: rotate(-55deg);transform-origin: center; fill:white;">
-                <textPath xlink:href="#circle">
-                  {{ user.connectedPaytacaAddress }}
-                </textPath>
-              </text>
-            </svg>
-          </div>
-        </q-item-label>
-        <SidebarMenu />
-      </q-list>
+      <div class="row justify-center q-gutter-sm q-pt-lg">
+        <div class="col-12 text-center">
+          <q-avatar>
+            <q-img src="images/bch-logo.png"></q-img>
+          </q-avatar>
+        </div>
+        <div class="col-12 text-center">
+          <q-chip color="grey">
+            {{ user.connectedPaytacaAddress?.replace(user.connectedPaytacaAddress.substring(11, 40), '...') }}
+          </q-chip>
+        </div>
+      </div>
+      <SidebarMenu />
     </q-drawer>
-
     <q-page-container>
       <StatusBar />
       <router-view />
@@ -51,27 +41,6 @@ import LightSwitch from 'components/LightSwitch.vue';
 import PaytacaConnect from 'components/PaytacaConnect.vue';
 import HeadLess from 'components/HeadLess.vue';
 import useStore from 'src/composables/useStore'
-
-const links = [
-  {
-    title: 'New Fungible Token',
-    caption: 'Create new fungible token',
-    icon: 'add',
-    link: '/ft/create'
-  },
-  {
-    title: 'New Non Fungible Token',
-    caption: 'Create new non-fungible token',
-    icon: 'add',
-    link: '/nft/create'
-  },
-  {
-    title: 'New Fungible Token',
-    caption: 'New fungible token',
-    icon: 'add',
-    link: '/ft/new'
-  }
-];
 
 export default defineComponent({
   name: 'MainLayout',
