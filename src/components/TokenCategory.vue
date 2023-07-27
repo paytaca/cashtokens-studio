d70ebdf6f11fdae2c858698394a4ee44ef36afd4
hantyrram <rongrammer@hotmail.com>
2023-07-27 19:42:33 +0800

    created token category component

---
+++ src/components/TokenCategory.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-btn @click.stop="() => copyText(tokenId)" flat dense rounded>
+    <code
+      :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-8'">{{ tokenId.replace(tokenId.substring(5, 60), '...') }}<q-tooltip>Click to copy</q-tooltip></code>
+  </q-btn>
+</template>
+<script setup lang="ts">
+import copyText from 'src/utils/copyText'
+defineOptions({ name: 'TokenCategory' })
+defineProps<{ tokenId: string }>()
+</script>
