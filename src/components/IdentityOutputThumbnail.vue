fcb4d6db77ee5a7a7e327df4343642a31d2eb096
hantyrram <rongrammer@hotmail.com>
2023-07-27 19:43:37 +0800

    used TokenCategory component,removed inline

--- src/components/IdentityOutputThumbnail.vue
+++ src/components/IdentityOutputThumbnail.vue
@@ -1,22 +1,17 @@
 <template>
   <q-card bordered class="token-thumbnail" @click="router.push('/token/view?tokenId=' + tokenId)">
     <q-card-title class="ellipsis justify-center row text-caption q-mt-xs" bordered>
-      <q-btn @click.stop="() => copyText(tokenId)" flat dense rounded>
-        <code>
-                                {{ tokenId.replace(tokenId.substring(5, 60), '...') }}
-                                <q-tooltip>Click to copy token id</q-tooltip>
-                              </code>
-      </q-btn>
+      <TokenCategory :token-id="tokenId" />
     </q-card-title>
     <q-card-section>
       <div class="row items-end justify-left">
         <div class="col-12">
           <q-skeleton v-if="!icon && loading" type="QAvatar"></q-skeleton>
           <q-avatar v-if="icon" square rounded>
-            <img :src="icon" alt="">
+            <img :src="icon" alt="NA" onerror="this.src='images/token.png'">
           </q-avatar>
           <q-avatar v-if="!icon && !loading" square rounded>
-            <q-icon name="token"></q-icon>
+            <q-icon name="token" size="lg"></q-icon>
           </q-avatar>
         </div>
         <div class="col-12 justify-center row q-mt-xs">
@@ -34,6 +29,7 @@
 
 import { useRouter } from 'vue-router'
 import copyText from 'src/utils/copyText'
+import TokenCategory from 'src/components/TokenCategory.vue'
 
 defineOptions({ name: 'IdentityOutputThumbnail' })
 const router = useRouter()
