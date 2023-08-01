1c171f8a7f7a92afcd06bf60765adf3b61625fec
hantyrram <rongrammer@hotmail.com>
2023-08-01 08:56:26 +0800

    added font assets

---
+++ src/components/TokenUnresolvedAvatar.vue
@@ -0,0 +1,8 @@
+<template>
+  <q-avatar icon="image_not_supported" font-size="2em" color="grey-8" style="width: 100px; height: 100px">
+    <q-tooltip>Unable to resolve token's icon from registry.</q-tooltip>
+  </q-avatar>
+</template>
+<script setup lang="ts">
+defineOptions({ name: 'TokenUnresolvedAvatar' })
+</script>
