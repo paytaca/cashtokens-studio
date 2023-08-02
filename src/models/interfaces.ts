899b1ed15f44b25ae3a4a4a6d7dc6eb2f4dc608b
hantyrram <rongrammer@hotmail.com>
2023-08-02 11:44:37 +0800

    added new interface

--- src/models/interfaces.ts
+++ src/models/interfaces.ts
@@ -1,3 +1,4 @@
+import { NFTCapability, Wallet } from "mainnet-js"
 
 export type Registry = {
   url:string,
@@ -8,7 +9,37 @@ type GenesisOptions = {
   recipient: string
 }
 
-export interface CashStudioTokenI {
+export type Message = {
+  type?: string,
+  text: string
+}
+
+export interface Processing {
+  get processing():string | undefined
+}
+
+export interface Messaging {
+  get message():Message | undefined
+}
+
+export interface GenesisCreator {
+  /**
+   * Create a token genesis
+   * @return {string} The transaction hash on success
+   */
+  createGenesis(opt: GenesisOptions): Promise<string|void>
+}
+
+export interface CashStudioTokenI extends Processing, Messaging{
+  tokenId?: string
+  amount?:string
+  capability?:NFTCapability
+  commitment?:string
+  registry?: Registry
+  ownerWallet?: Wallet
+}
+
+export interface AuthchainIdentity {
   /**
    * Create a token genesis
    * @return {string} The transaction hash on success
