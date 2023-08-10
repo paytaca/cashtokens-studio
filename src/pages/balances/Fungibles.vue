0c17a615eb3ed58cf03a20e79ab0b76ddd7fcb0a
hantyrram <rongrammer@hotmail.com>
2023-08-10 17:18:16 +0800

    consolidate amount of each ft instead of displaying each utxo

--- src/pages/balances/Fungibles.vue
+++ src/pages/balances/Fungibles.vue
@@ -9,29 +9,23 @@
               <tr>
                 <th>#</th>
                 <th>Token Id</th>
-                <th>Amount</th>
+                <th>Balance</th>
+                <th>Utxo Count</th>
                 <th>Action</th>
               </tr>
             </thead>
             <TableBodySkeleton v-if="AuthchainIdentity.processing" :col-count="8" :row-count="3"
               :caption="AuthchainIdentity.processing" />
             <tbody v-else class="text-center">
-              <tr v-for="t, i in tokens" :key="'ai-rec-' + i">
+              <tr v-for="b, i in balances" :key="'ai-rec-' + i">
                 <td>{{ i + 1 }}</td>
                 <td>
-                  <TokenCategory :tokenId="t.token?.tokenId" />
+                  <TokenCategory :tokenId="b.tokenId" />
                 </td>
-                <td>{{ t.token?.amount || 'n/a' }}</td>
+                <td>{{ b.balance || 0 }}</td>
+                <td>{{ b.sourceUtxos.length }}</td>
                 <td>
-                  <q-btn icon="more_vert" size="md" round flat dense>
-                    <q-menu>
-                      <q-list>
-                        <!-- <q-item clickable v-close-popup @click="openDialog(FungibleTokenIssuerDialog.__name, t)">
-                          Issue Tokens
-                        </q-item> -->
-                      </q-list>
-                    </q-menu>
-                  </q-btn>
+                  <q-btn color="secondary" dense no-caps>Send</q-btn>
                 </td>
               </tr>
             </tbody>
@@ -54,14 +48,21 @@ import TableBodySkeleton from 'src/components/TableBodySkeleton.vue'
 import FungibleToken from 'src/models/FungibleToken';
 defineOptions({ name: 'FungibleTokens' })
 const user = useUser()
-const tokens = ref<FungibleToken[]>()
 const { dialog, dialogData, openDialog, onHide } = useDialogs()
-
+const balances = ref<{ tokenId: string, sourceUtxos: UtxoI[], balance: bigint }[]>([])
 onMounted(async () => {
   if (user.wallet) {
-    tokens.value = []
     const utxos = (await user.wallet.getAddressUtxos()).filter((u: UtxoI) => u.token && u.token?.amount > 0)
-    utxos.forEach((f) => tokens.value.push(new FungibleToken({ ...f, ownerWallet: user.wallet as Wallet })))
+    utxos.forEach((u: UtxoI) => {
+      let b = balances.value.find((b) => b.tokenId === u.token?.tokenId)
+      if (b) {
+        b.sourceUtxos.push(u)
+        b.balance += BigInt(u.token!.amount)
+      } else {
+        balances.value.push({ tokenId: u.token!.tokenId, sourceUtxos: [u], balance: BigInt(u.token!.amount) })
+      }
+    })
+
   }
 })
 </script>
