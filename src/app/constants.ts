901e1b07289c0c08f73a247b540553eb37bbfb60
ubuntu <rongrammer@hotmail.com>
2023-09-21 15:05:06 +0800

    defined new constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,6 @@
 export const DEFAULT_TOKEN_VALUE = 1000
 export const MIN_TOKEN_VALUE = 800
 export const MAX_FUNGIBLE_AMOUNT = '9223372036854770000' // - 9223372036854775807 - 5807 number limit, change when mainnet-js uses bigint
+export const CTS_MINTING_TOKEN_DEFAULT_COMMITMENT = 'feed' // actual default commitment value of minters
+export const CTS_MINTING_TOKEN_DEFAULT_DUMMY_COMMITMENT = 'ctsfeed782974771942$%&*@(&#(@&' // default commitment value in forms, because the user might actually need to use `feed`
+
