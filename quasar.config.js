d22fb28409c4ed1ff2c4b52ab9d6026f0d3cf0c0
ubuntu <rongrammer@hotmail.com>
2023-09-22 13:38:17 +0800

    enabled eventBus boot file

--- quasar.config.js
+++ quasar.config.js
@@ -44,8 +44,7 @@ module.exports = configure(function (ctx) {
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-webpack/boot-files
     boot: [
-
-
+      'eventbus.ts'
     ],
 
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-css
