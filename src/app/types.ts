64245b6e13e2e3525dc14c2ebe01f79267ba23de
ubuntu <rongrammer@hotmail.com>
2023-09-22 13:39:33 +0800

    updated GenesisOptions  type

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,13 +1,17 @@
 import { NFTCapability, TokenI, UtxoI } from "mainnet-js"
 import { TokenCategory, URIs } from "./bcmr/bcmr-v2.schema"
 
+export type NftCollectionType = 'SequentialNftCollection' | 'ParsableNftCollection' //
+
 export type GenesisOptions = {
   useAuthGuard?: boolean,                                 // use authguard, default = true
   includeAuthKeyGenesis?: boolean,                        // also create AuthKey genesis, useAuthGuard will be ignored and assumes to be true
-  issuedSupply?: { amount: string, recipient: string }, // the token amount to issue
+  issuedSupply?: { amount: string, recipient: string },   // the token amount to issue
   amount?: string | number,
   capability?: NFTCapability,
-  commitment?: string
+  commitment?: string,                                    // could be a number text e.g. '10' or hex '0a' Big Endian
+  commitmentFormat?: 'decimal'|'hex'                      // what's the format of the commitment's value
+  nftCollectionType?: NftCollectionType
 }
 
 export type TokenBalance = {
