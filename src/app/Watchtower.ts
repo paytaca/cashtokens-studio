b6375188e1621abb65945365e2c9fb600c0dea4d
ubuntu <rongrammer@hotmail.com>
2023-10-05 15:12:45 +0800

    added method to fetch bch balance from watchtower

--- src/app/Watchtower.ts
+++ src/app/Watchtower.ts
@@ -17,6 +17,13 @@ type FetchAuthchainIdentitiesQueryParams = {
   authguard?: string
 } & PaginationQueryParams
 
+type BchBalance = {
+  valid?:boolean,
+  address?: string,
+  spendable?: number,
+  balance?: number
+}
+
 export type FetchUtxoQueryParams = {
   is_token?: boolean,
   token_type?: 'ft' | 'nft' | 'hybrid',
@@ -144,6 +151,21 @@ export class Watchtower {
         delete this.processing
       }
       return result
+    } 
+
+    /**
+     * Get the address' BCH balance from watchtower
+     */
+    async fetchBchBalance(ownerAddress: string): Promise<BchBalance> {
+      try {
+        const r = await fetch(`${this.apiBaseUri}balance/bch/${ownerAddress}`)
+        return await r.json()
+      } catch (error) {
+        this.error = error
+      } finally {
+        delete this.processing
+      }
+      return {}
     } 
 
   
