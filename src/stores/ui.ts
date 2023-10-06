8b043c0718c641fd5b4535fe097296695913ed45
ubuntu <rongrammer@hotmail.com>
2023-10-06 16:38:51 +0800

    updated ui store

--- src/stores/ui.ts
+++ src/stores/ui.ts
@@ -2,7 +2,9 @@ import { defineStore } from 'pinia'
 import { AuthchainIdentity, CashToken } from 'src/app';
 
 type UIState = {
-  statusMessage?: string,
+  statusMessage: string,
+  statusMessageType: 'info'|'error'|'success'|'warning'|'',
+  statusMessageSpinner: boolean,
   transactionLogs?: any[],
   /**
    * The token that will loaded in Token Page
@@ -11,5 +13,21 @@ type UIState = {
 }
 
 export const useUI = defineStore('ui', {
-  state: (): UIState => ({})
+  state: (): UIState => ({
+    statusMessage: '',
+    statusMessageType:'',
+    statusMessageSpinner: false 
+  }),
+  actions: {
+    clearStatusMessage() {
+      this.statusMessage = ''
+      this.statusMessageType = ''
+      this.statusMessageSpinner = false
+    },
+    setStatusMessage(m: {statusMessage:string, statusMessageType?: 'info'|'error'|'success'|'warning', statusMessageSpinner?:boolean}) {
+      this.statusMessage = m.statusMessage
+      this.statusMessageType = m.statusMessageType || 'success'
+      this.statusMessageSpinner = m.statusMessageSpinner || false
+    }
+  }
 });
