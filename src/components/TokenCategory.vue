8fcd6749989fc9aff5dbd7646abd19a46672e0d4
ubuntu <rongrammer@hotmail.com>
2023-10-18 09:39:17 +0800

    fix ui inconsistencies when entering a float value on amount and there is no decimals metadata

--- src/components/TokenCategory.vue
+++ src/components/TokenCategory.vue
@@ -4,14 +4,14 @@
     :icon-right="copied && iconRight ? 'done_all' : iconRight || undefined" color="warning"
     style="white-space: nowrap;min-width: 120px">
     <code v-if="tokenId">
-                <q-icon v-if="copied && !iconRight" name="done_all" :color="$q.dark.isActive ? 'grey-1' : 'grey-10'"></q-icon>
-                {{ tokenId.replace(tokenId.substring(5, 60), '...') }}
-                <q-tooltip>Copy Token ID</q-tooltip>
-              </code>
+                    <q-icon v-if="copied && !iconRight" name="done_all" :color="$q.dark.isActive ? 'grey-1' : 'grey-10'"></q-icon>
+                    {{ tokenId.replace(tokenId.substring(5, 60), '...') }}
+                    <q-tooltip>Copy Token ID</q-tooltip>
+                  </code>
     <code v-else :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-8'">
-                {{ '...' }}
-                  <q-tooltip>N/A</q-tooltip>
-              </code>
+                    {{ '...' }}
+                      <q-tooltip>N/A</q-tooltip>
+                  </code>
   </q-btn>
 </template>
 <script setup lang="ts">
