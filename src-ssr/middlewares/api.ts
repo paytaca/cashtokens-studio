55249282946c76f67e89d4fc13e12471dac92d77
ubuntu <rongrammer@hotmail.com>
2024-01-25 15:04:14 +0800

    updated response for file upload

--- src-ssr/middlewares/api.ts
+++ src-ssr/middlewares/api.ts
@@ -88,7 +88,7 @@ export default ssrMiddleware(async ({ app, resolve }) => {
     
   })
 
-  app.post('/api/tokens/nft/asset-upload', upload.single('icon'), async (req:any, res:any) => {
+  app.post('/api/tokens/nft/asset-upload', upload.single('file'), async (req:any, res:any) => {
     let ext = req.file.originalname?.split('.')
     ext = ext[ext.length - 1]
     const metadata = await nftStorageClient().store({
@@ -107,7 +107,7 @@ export default ssrMiddleware(async ({ app, resolve }) => {
       const [imageCid, imageFilename] = image.replace('ipfs://','').split('/')
       res.status(200).send({
         nftStorageMetadata: metadata,
-        iconUris: {
+        assetUris: {
           ipfs: image,
           https: `https://${imageCid}.ipfs.nftstorage.link/${imageFilename}`
         }
