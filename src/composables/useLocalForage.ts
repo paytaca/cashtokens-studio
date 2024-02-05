c336e7da5312767ff635f3d46c6869a12b7de165
ubuntu <rongrammer@hotmail.com>
2024-02-05 17:13:26 +0800

    create storage instance in forage

--- src/composables/useLocalForage.ts
+++ src/composables/useLocalForage.ts
@@ -6,10 +6,16 @@ export function useLocalForage() {
   // Create a localforage instance with Quasar-specific options for offline support
   const nftTypesStore = localforage.createInstance({
     name: 'cts-nfttypes',
-    storeName: 'cts', // Customize store name if needed
+    storeName: 'cts', 
+    driver: [localforage.INDEXEDDB, localforage.LOCALSTORAGE, localforage.WEBSQL] // Use multiple drivers for fallback
+  });
+
+  const pageStore = localforage.createInstance({
+    name: 'cts-page',
+    storeName: 'cts', 
     driver: [localforage.INDEXEDDB, localforage.LOCALSTORAGE, localforage.WEBSQL] // Use multiple drivers for fallback
   });
 
 
-  return { nftTypesStore };
+  return { nftTypesStore, pageStore };
 }
