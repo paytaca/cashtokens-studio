a374f1ddb2cbc5a0c42aee188a08be4909d434fe
ubuntu <rongrammer@hotmail.com>
2024-02-05 17:14:33 +0800

    force dark mode

--- quasar.config.js
+++ quasar.config.js
@@ -183,7 +183,7 @@ module.exports = configure(function (ctx) {
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-framework
     framework: {
       config: {
-        dark: 'auto',
+        dark: true,
         notify: {}
       },
 
