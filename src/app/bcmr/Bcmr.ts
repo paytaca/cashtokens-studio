ae1310cc18855a261742ffe2d04957f89e51f99f
ubuntu <rongrammer@hotmail.com>
2024-02-05 09:55:36 +0800

    handle http 400 error

--- src/app/bcmr/Bcmr.ts
+++ src/app/bcmr/Bcmr.ts
@@ -344,10 +344,14 @@ export class Bcmr implements Registry {
         method: 'POST', body: this.getContent(),
         headers: { 'Content-Type': 'application/json' }
       })
+      if (resp.status >= 400) {
+        throw new Error('Error, storing registry in IPFS, please try again later.')
+      }
       const respJson = await resp.json()
       return respJson.artifact
     } catch (error) {
       console.log(error)
+      throw error 
     } finally {
       delete this._processing
     }
