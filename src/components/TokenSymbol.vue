b134315c38fa2add7bcc608e15ad0f82c54caf9f
ubuntu <rongrammer@hotmail.com>
2024-02-15 15:06:16 +0800

    updated token symbol style

--- src/components/TokenSymbol.vue
+++ src/components/TokenSymbol.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-chip color="primary" class="q-p-sm text-bold" square outline :label="symbol" />
+  <q-chip color="primary" class="q-p-sm text-bold" square outline :label="symbol" style="letter-spacing: 5px;" />
 </template>
 <script setup lang="ts">
 defineProps<{ symbol: string }>()
