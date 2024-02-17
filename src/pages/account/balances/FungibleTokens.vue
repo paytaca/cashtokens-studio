9569dd26a6ad9e034212f5310f2d2148b162256c
ubuntu <rongrammer@hotmail.com>
2024-02-17 12:29:12 +0800

    redesigned fungible token balances view

--- src/pages/account/balances/FungibleTokens.vue
+++ src/pages/account/balances/FungibleTokens.vue
@@ -1,232 +1,225 @@
 <template>
-  <q-page class="q-ma-lg">
-    <div class="row justify-center q-mx-sm">
+  <q-page class="q-ma-sm">
+    <div class="row justify-center">
       <div class="col-xs-12 col-md-10">
-        <h5 class="text-center">Fungible Token Balances</h5>
-        <div class="q-pa-lg flex flex-center">
-          <q-pagination v-model="pagination.currentPage" :max="pagination.numberOfPages"
-            :max-pages="pagination.maxRowsPerPage" :boundary-numbers="false" />
+        <h5 class="text-center">
+          My Fungible Tokens
+          <q-badge color="blue-5" text-color="black" align="top" rounded>
+            {{ ftBalances?.count || 0 }}
+          </q-badge>
+        </h5>
+        <div>
+          <q-table v-model:pagination="pagination" @request="onTableRequest" flat bordered :rows="ftBalances.results"
+            :columns="[
+              {
+                name: 'icon', label: 'Icon',
+                field: r => r.identitySnapshot?.uris?.icon || '<not found>',
+                align: 'center',
+                headerStyle: 'padding: 1.5em'
+              },
+              {
+                name: 'tokenid', label: 'Category',
+                field: 'tokenid',
+                align: 'center',
+                headerStyle: 'padding: 1.5em'
+              },
+              {
+                name: 'decimals', label: 'Decimals',
+                field: r => !r.identitySnapshot?.token ? '<metadata not found>' : r.identitySnapshot?.token?.decimals || 0,
+                align: 'center',
+                headerStyle: 'padding: 1.5em',
+                style: 'font-size: 1em;',
+                classes: r => r.identitySnapshot?.token?.decimals ? 'ellipsis text-warning' : 'ellipsis'
+              },
+              {
+                name: 'balance', label: 'Balance',
+                field: 'balance',
+                align: 'center',
+                headerStyle: 'padding: 1.5em',
+                style: 'font-size: 1em;font-weight: bold',
+                classes: 'ellipsis'
+              },
+              {
+                name: 'actions', label: 'Send',
+                field: r => '',
+                align: 'center',
+                headerStyle: 'padding: 1.5em'
+
+              }
+            ]" :rows-per-page-options="rowsPerPageOptions" row-key="name" :visible-columns="visibleColumns">
+
+            <template v-slot:body-cell-icon="value">
+              <q-td class="text-center">
+                <q-avatar v-if="value.row.identitySnapshot?.uris?.icon">
+                  <q-img :src="value.row.identitySnapshot.uris.icon" />
+                </q-avatar>
+                <q-icon v-else name="money" size="xl" color="grey-8"></q-icon>
+              </q-td>
+            </template>
+
+            <template v-slot:body-cell-tokenid="value">
+              <q-td class="text-center">
+                <TokenCategory :tokenId="value.row.tokenId" />
+              </q-td>
+            </template>
+            <template v-slot:body-cell-balance="value">
+              <q-td>
+                <div class="row justify-evenly">
+                  <div class="col text-positive text-right"
+                    style="font-variant-numeric: tabular-nums; letter-spacing: 2px;">
+                    {{ ftAmountFormatter.toDecimal(value.row.balance.toString(),
+                      value.row.identitySnapshot?.token?.decimals).replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                    }} </div>
+
+                  <div class="col text-bold text-left q-pl-sm" style="letter-spacing: 2px;">({{
+                    value.row.identitySnapshot?.token?.symbol }})</div>
+                </div>
+              </q-td>
+            </template>
+            <template v-slot:body-cell-actions="value">
+              <q-td class="text-center">
+                <q-btn text-color="primary" icon="send" dense no-caps
+                  @click="openDialog(FTBalanceTransferDialog.__name, value.row)"
+                  :disable="value.row.balance > Number.MAX_SAFE_INTEGER" size="md"></q-btn>
+              </q-td>
+            </template>
+          </q-table>
         </div>
-        <q-scroll-area style="position:relative; height: 100vh; max-width: 100vw;" :bar-style="{ width: '0px' }">
-          <q-markup-table>
-            <thead>
-              <tr v-if="watchtower.processing">
-                <th colspan="6">
-                  <q-spinner-grid size="xs"></q-spinner-grid> Loading
-                </th>
-              </tr>
-              <tr>
-                <th>#</th>
-                <th>Brand</th>
-                <th>Symbol</th>
-                <th>Token Id</th>
-                <th>Balance</th>
-                <!-- <th>Utxo Count</th> -->
-                <th>Action</th>
-              </tr>
-            </thead>
-            <TableBodySkeleton v-if="watchtower.processing && !ftBalances" :col-count="4" :row-count="3"
-              :caption="watchtower.processing" />
-            <tbody v-else class="text-center">
-              <tr v-for="b, i in ftBalances" :key="'ai-rec-' + i">
-                <td>{{ i + pagination.offset + 1 }}</td>
-                <td>
-                  <q-avatar v-if="b.tokenUris?.icon">
-                    <img :src="b.tokenUris?.icon" alt="na">
-                  </q-avatar>
-                  <q-icon v-else name="token" size="xl" color="disabled" />
-                </td>
-                <td>
-                  <q-spinner v-if="bcmrIndexer.processing && !b.tokenCategory?.symbol"></q-spinner>
-                  <div v-else>
-                    <q-chip v-if="b.tokenCategory?.symbol" color="primary" class="q-p-sm" square outline>
-                      {{ b.tokenCategory?.symbol }}
-                    </q-chip>
-                    <span v-else>---</span>
-                  </div>
-                </td>
-                <td>
-                  <TokenCategory :tokenId="b.tokenId" />
-                </td>
-                <td>
-                  <q-btn flat dense @click.stop="watchtower.subscribe(user.walletTokenAddress)">
-                    {{ ftAmountFormatter.toDecimal(b.balance.toString(), b.tokenCategory?.decimals) }}
-                  </q-btn>
-                </td>
-                <!-- <td>{{ b.utxoCount }}</td> -->
-                <td>
-                  <q-btn color="primary" dense no-caps @click="openDialog(FTBalanceTransferDialog.__name, b)"
-                    :disable="b.balance > Number.MAX_SAFE_INTEGER">Send</q-btn>
-                </td>
-              </tr>
-              <tr v-if="ftBalances?.length === 0 && !watchtower.processing">
-                <td colspan="6">
-                  No data
-                </td>
-              </tr>
-            </tbody>
-          </q-markup-table>
-          <FTBalanceTransferDialog :model-value="dialog === FTBalanceTransferDialog.__name" :token-balance="dialogData"
-            @hide="onHide" @ft-transferred="() => onFTTransferred()" />
-        </q-scroll-area>
       </div>
     </div>
+    <FTBalanceTransferDialog :model-value="dialog === FTBalanceTransferDialog.__name" :token-balance="dialogData"
+      @hide="onHide" @ft-transferred="() => onFTTransferred()" />
   </q-page>
 </template>
 <script setup lang="ts">
-import { inject, onBeforeUnmount, onMounted, ref, watch } from 'vue';
-import { useUser } from 'src/stores/user';
+import { onMounted, ref, watch, computed, inject, onBeforeUnmount, onBeforeMount } from 'vue';
+import { useUser } from 'src/stores/user'
 import { useDialogs } from 'src/composables'
-import { ADDRESS_WATCHER_TRIGGERED, TOKEN_CATEGORY_CACHE_MAX_KEYS, TOKEN_URIS_CACHE_MAX_KEYS, Watchtower } from 'src/app'
-import TokenCategory from 'src/components/TokenCategory.vue'
-import TableBodySkeleton from 'src/components/TableBodySkeleton.vue'
-import { FungibleTokenBalance, PaginatedData } from 'src/app/types';
-import { BcmrIndexer } from 'src/app/bcmr/BcmrIndexer';
-import { getWalletClass, tokeshiToNumber } from 'src/app/utils';
+import { ADDRESS_WATCHER_TRIGGERED, AuthKey, BcmrIndexer, CashToken, Watchtower } from 'src/app'
+import { FungibleTokenBalance, PaginatedData, TransactionSigner } from 'src/app/types';
+import { FetchUtxoQueryParams } from 'src/app/Watchtower'
 import FTBalanceTransferDialog from 'src/components/dialogs/FTBalanceTransferDialog.vue';
-import { EventBus } from 'quasar';
-import { useUI } from 'src/stores/ui';
-import { delay } from 'mainnet-js';
-import ftAmountFormatter from 'src/app/utils/ftAmountFormatter';
-
-
-defineOptions({ name: 'FungibleTokens' })
-
+import { IdentitySnapshot, URIs, UtxoI, Wallet } from 'mainnet-js';
+import { formatCommitment, ipfsToGatewayUrl, shortenTokenId } from 'src/app/utils';
+import { EventBus, useQuasar } from 'quasar';
+import TokenCategory from 'src/components/TokenCategory.vue'
+import { Console } from 'console';
+import ftAmountFormatter from 'src/app/utils/ftAmountFormatter'
+defineOptions({ name: 'NonFungibleTokens' })
+const $q = useQuasar()
 const user = useUser()
-const ui = useUI()
-const eventBus = inject<EventBus>('eventBus')
 const { dialog, dialogData, openDialog, onHide, hideDialog } = useDialogs()
-const ftBalances = ref<FungibleTokenBalance[]>([])
-const paginatedFtBalances = ref<PaginatedData>({
+
+const visibleColumns = computed(() => {
+  if ($q.screen.lt.sm) {
+    return ['icon', 'balance', 'actions']
+  }
+  return ['icon', 'tokenid', 'decimals', 'balance', 'actions']
+})
+
+const ftBalances = ref<PaginatedData>({
   count: 0,
-  limit: 10,
+  limit: 0,
   offset: 0,
-  next: '',
-  previous: '',
+  next: null,
+  previous: null,
   results: []
 })
-const watchtower = ref<Watchtower>(new Watchtower())
-const pagination = ref<{ numberOfPages: number, currentPage: number, maxRowsPerPage: number, rowCount: number, offset: number }>({
-  numberOfPages: 0,
-  currentPage: 0,
-  maxRowsPerPage: 10,
-  rowCount: 0,
-  offset: 0,
+
+const pagination = ref({
+  sortBy: 'desc',
+  descending: false,
+  page: 1,
+  rowsPerPage: 12,
+  rowsNumber: 12
 })
 
-const bcmrIndexer = ref<BcmrIndexer>(new BcmrIndexer())
-
-const populateFtBalances = (paginated: PaginatedData) => {
-  // populate 
-  ftBalances.value = []
-  const results = paginated?.results || []
-  for (let i = 0; i < results.length; i++) {
-    const ftBalance: FungibleTokenBalance = results[i]
-    if (ui.tokenCategoryCache[ftBalance.tokenId]) {
-      ftBalance.tokenCategory = ui.tokenCategoryCache[ftBalance.tokenId]
-    }
-    if (ui.tokenUrisCache[ftBalance.tokenId]) {
-      ftBalance.tokenUris = ui.tokenUrisCache[ftBalance.tokenId]
-    }
-    ftBalances.value.push(ftBalance)
-  }
-  ftBalances.value.forEach(async (a) => {
-    if (!ui.tokenCategoryCache[a.tokenId]) {
-      a.tokenCategory = await bcmrIndexer.value.fetchToken(a.tokenId)
-      if (a.tokenCategory && Object.keys(ui.tokenCategoryCache).length < TOKEN_CATEGORY_CACHE_MAX_KEYS) {
-        ui.tokenCategoryCache[a.tokenId] = a.tokenCategory
-      }
-    } else {
-      a.tokenCategory = ui.tokenCategoryCache[a.tokenId]
-    }
-    if (!ui.tokenUrisCache[a.tokenId]) {
-      a.tokenUris = await bcmrIndexer.value.fetchTokenUris(a.tokenId)
-      if (a.tokenCategory && Object.keys(ui.tokenCategoryCache).length < TOKEN_URIS_CACHE_MAX_KEYS) {
-        ui.tokenCategoryCache[a.tokenId] = a.tokenCategory
-      }
-    } else {
-      a.tokenUris = ui.tokenUrisCache[a.tokenId]
-    }
-    // a.tokenCategory = await bcmrIndexer.value.fetchToken(a.tokenId)
-    // a.tokenUris = await bcmrIndexer.value.fetchTokenUris(a.tokenId)
-  })
-}
-
-watch(() => pagination.value.currentPage, async (pageNumber, oldPageNumber) => {
-  if (user.wallet) {
-    pagination.value.offset = (pageNumber - 1) * pagination.value.maxRowsPerPage
-    paginatedFtBalances.value = await watchtower.value.fetchFtBalance(
-      user.wallet.getTokenDepositAddress(), { limit: pagination.value.maxRowsPerPage, offset: pagination.value.offset }
-    )
-    // populate 
-    populateFtBalances(paginatedFtBalances.value)
-    user.paginatedFtBalances = paginatedFtBalances.value
-  }
+const rowsPerPageOptions = computed(() => {
+  return [12, 24, 36]
 })
-
-const initPagination = () => {
-  if (paginatedFtBalances.value && paginatedFtBalances.value?.count > 0) {
-    pagination.value.currentPage = Math.ceil((paginatedFtBalances.value.offset + 1) / paginatedFtBalances.value.limit)
-    pagination.value.maxRowsPerPage = paginatedFtBalances.value.limit
-    pagination.value.rowCount = paginatedFtBalances.value.count
-    pagination.value.numberOfPages = Math.ceil(paginatedFtBalances.value.count / paginatedFtBalances.value.limit)
-    pagination.value.offset = paginatedFtBalances.value.offset
-  }
-}
-
-const refreshData = async (immediate?: boolean) => {
-  if (!immediate) {
-    await delay(2500)
-  }
-  if (user.wallet) {
-    paginatedFtBalances.value = await watchtower.value.fetchFtBalance(
-      user.wallet.getTokenDepositAddress(),
-      { limit: pagination.value.maxRowsPerPage, offset: pagination.value.offset }
-    )
-
-    user.paginatedFtBalances = paginatedFtBalances.value
-    populateFtBalances(paginatedFtBalances.value)
-    initPagination()
-  }
-}
 
 const onFTTransferred = () => {
   hideDialog()
-  refreshData()
 }
 
-watch(() => user.walletAddress, async (v) => {
-  if (v) {
-    // keep so page survives reload
-    user.wallet = await getWalletClass().watchOnly(v)
-    refreshData()
-    eventBus?.on(ADDRESS_WATCHER_TRIGGERED, () => {
-      refreshData()
-    })
-  } else {
-    eventBus?.off(ADDRESS_WATCHER_TRIGGERED)
+// TODO: refactor, remove this we just used this to test
+class FungibleTokenBalanceImpl implements FungibleTokenBalance {
+  tokenId: string
+  utxoCount: number
+  balance: bigint
+  owner: string
+  utxos?: UtxoI[]
+  identitySnapshot?: IdentitySnapshot
+  processing?: string
+  constructor(arg: { tokenId: string, utxoCount: number, balance: bigint, owner: string, utxos?: UtxoI[], identitySnapshot?: IdentitySnapshot }) {
+    this.tokenId = arg.tokenId
+    this.utxos = arg.utxos
+    this.utxoCount = arg.utxoCount
+    this.balance = arg.balance
+    this.owner = arg.owner
+    this.identitySnapshot = arg.identitySnapshot
   }
 
-})
+  async resolveIdentitySnapshot(quite?: boolean) {
+    if (!this.tokenId) return
+    try {
+      if (quite !== true) {
+        this.processing = 'Downloading metadata'
+      }
+      const r = await (new BcmrIndexer()).fetchIdentitySnapshot(this.tokenId)
+      this.identitySnapshot = r
 
-onMounted(async () => {
-
-  if (user.wallet) {
-    /**
-     * Load from store by default then refresh
-     */
-    if (user.paginatedFtBalances) {
-      paginatedFtBalances.value = user.paginatedFtBalances
-      populateFtBalances(paginatedFtBalances.value)
+    } catch (error: any) {
+    } finally {
+      this.processing = ''
     }
-    refreshData()
   }
-  eventBus?.on(ADDRESS_WATCHER_TRIGGERED, () => {
-    refreshData()
-  })
+
+}
+
+
+const populateftBalances = async (wallet: Wallet) => {
+  if (wallet) {
+    const query: FetchUtxoQueryParams = { limit: pagination.value.rowsPerPage, offset: (pagination.value.page - 1) * pagination.value.rowsPerPage }
+    $q.loading.show()
+    const resp = await (new Watchtower()).fetchFtBalance(
+      wallet.getTokenDepositAddress(),
+      query
+    )
+    $q.loading.hide()
+
+    if (resp?.count > 0) {
+      ftBalances.value = resp
+      pagination.value.rowsNumber = resp.count
+      ftBalances.value.results?.forEach(async (ft: FungibleTokenBalance, i) => {
+        ftBalances.value.results[i] = new FungibleTokenBalanceImpl({ ...ft })
+        await ftBalances.value.results[i].resolveIdentitySnapshot()
+      })
+    }
+
+  }
+}
+
+onBeforeMount(async () => {
+  if (user.wallet) {
+    await populateftBalances(user.wallet as Wallet)
+  }
+
 })
 
-onBeforeUnmount(() => {
-  eventBus?.off(ADDRESS_WATCHER_TRIGGERED)
-})
+const onTableRequest = async (props: any) => {
+  pagination.value = props.pagination
+  await populateftBalances(user.wallet as Wallet)
+}
+
+// onMounted(async () => {
+//   eventBus?.on(ADDRESS_WATCHER_TRIGGERED, () => {
+//     // refreshData()
+//   })
+// })
+
+// onBeforeUnmount(() => {
+//   eventBus?.off(ADDRESS_WATCHER_TRIGGERED)
+// })
+
 </script>
