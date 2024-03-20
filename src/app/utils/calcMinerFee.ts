80eb084daed2d4f637377ba3bc2e7840b980f06b
ubuntu <rongrammer@hotmail.com>
2024-03-20 17:53:18 +0800

    increased miner fee, tested bulk mint

--- src/app/utils/calcMinerFee.ts
+++ src/app/utils/calcMinerFee.ts
@@ -1,6 +1,6 @@
-import getByteCount from "./getByteCount"
+import getByteCount from './getByteCount';
 
-export default (inputs:any, outputs:any) => {
-  const b = getByteCount(inputs, outputs)
-  return Math.ceil(b * 1.1) + 600 // relayFee allowance
-}
+export default (inputs: any, outputs: any) => {
+  const b = getByteCount(inputs, outputs);
+  return Math.ceil(b * 1.4) + 2000; // relayFee allowance
+};
