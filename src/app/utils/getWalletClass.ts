f7883f6ffe922aab0ddeaf9e2f75c1e0ad8b0128
ubuntu <rongrammer@hotmail.com>
2025-01-06 09:20:34 +0800

    add testnet provider, imaginary.cash inaccessibility fallback

--- src/app/utils/getWalletClass.ts
+++ src/app/utils/getWalletClass.ts
@@ -1,13 +1,19 @@
-import { Wallet, TestNetWallet, DefaultProvider} from 'mainnet-js';
+import { Wallet, TestNetWallet, DefaultProvider } from 'mainnet-js';
 // DefaultProvider.servers.testnet = ["wss://chipnet.imaginary.cash:50004"];
 // DefaultProvider.servers.mainnet = ["wss://fulcrum.pat.mn:50004"];
 // DefaultProvider.servers.testnet = ['wss://blackie.c3-soft.com:64004'];
+DefaultProvider.servers.testnet = [
+  'wss://chipnet.c3-soft.com:64004',
+  'wss://chipnet.imaginary.cash:50004',
+];
 
 export default () => {
   let WalletClass = Wallet;
-  if (process.env.APP_ENV === 'development' || process.env.APP_ENV === 'development-build') {
+  if (
+    process.env.APP_ENV === 'development' ||
+    process.env.APP_ENV === 'development-build'
+  ) {
     WalletClass = TestNetWallet;
   }
-  return WalletClass
-}
-
+  return WalletClass;
+};
