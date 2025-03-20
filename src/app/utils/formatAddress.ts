f525bf04e9f76d82b73019ac4bccb7138097be45
ubuntu <rongrammer@hotmail.com>
2025-03-20 16:02:21 +0800

    replace hardcoded type and prefix when formatting test address to allow p2sh address

--- src/app/utils/formatAddress.ts
+++ src/app/utils/formatAddress.ts
@@ -1,12 +1,25 @@
-import { CashAddressNetworkPrefix, CashAddressType, decodeCashAddress, encodeCashAddress } from '@bitauth/libauth';
-export default (address:string): string => {
-  if (process.env.APP_ENV === 'development' || process.env.APP_ENV === 'development-build') {
+import {
+  CashAddressNetworkPrefix,
+  CashAddressType,
+  decodeCashAddress,
+  encodeCashAddress,
+} from '@bitauth/libauth';
+export default (address: string): string => {
+  if (
+    process.env.APP_ENV === 'development' ||
+    process.env.APP_ENV === 'development-build'
+  ) {
     const decoded = decodeCashAddress(address);
-    if (typeof decoded === "string") {
+    if (typeof decoded === 'string') {
       return '';
     }
-    const testAddress = encodeCashAddress(CashAddressNetworkPrefix.testnet, CashAddressType.p2pkh, decoded.payload);
+
+    const testAddress = encodeCashAddress(
+      decoded.prefix as CashAddressNetworkPrefix.testnet,
+      decoded.type,
+      decoded.payload
+    );
     return testAddress;
   }
   return address;
-} 
+};
