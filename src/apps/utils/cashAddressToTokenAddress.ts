209d289721360130c529d55b245209d6bf4b21e1
Ronaldo Ramano <ronskie@mac.local>
2025-07-16 10:40:31 +0800

    refactor cashaddr to tokenaddr converter util

--- src/apps/utils/cashAddressToTokenAddress.ts
+++ src/apps/utils/cashAddressToTokenAddress.ts
@@ -1,17 +1,14 @@
 import {
     CashAddressResult,
     cashAddressToLockingBytecode,
-    decodeCashAddress,
-    DecodedCashAddress,
     DecodedCashAddressLockingBytecode,
     lockingBytecodeToCashAddress
 } from "bitauth-libauth-v3"
 
-export const deriveTokenAddressFromCashAddress = (cashAddress: string) => {
-    const decoded = decodeCashAddress(cashAddress) as DecodedCashAddress
+export const cashAddressToTokenAddress = (cashAddress: string) => {
     const lockingBytecode = cashAddressToLockingBytecode(cashAddress) as DecodedCashAddressLockingBytecode
     const address =  lockingBytecodeToCashAddress({
-        prefix: decoded.prefix, bytecode: lockingBytecode.bytecode, tokenSupport: true
+        prefix: lockingBytecode.prefix, bytecode: lockingBytecode.bytecode, tokenSupport: true
     }) as CashAddressResult
     return address.address
 }
