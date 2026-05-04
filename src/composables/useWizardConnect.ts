cb4feb2d8a1d814ff315b5d62c7b1430726d82d3
hantyrram <rongrammer@hotmail.com>
2026-05-04 15:29:21 +0800

    breakdown wz wallet initialization process

--- src/composables/useWizardConnect.ts
+++ src/composables/useWizardConnect.ts
@@ -9,13 +9,14 @@ import { onMounted, ref } from 'vue';
 import { getDappMgr } from 'src/apps/wizard-connect/connection-manager';
 import QrCodeModal from 'src/components/wizard-connect/QrCodeModal.vue';
 import { getHDWalletClass } from 'src/apps/utils';
+import { StoredSession } from '@wizardconnect/dapp';
 
 type WZWalletPath = { name: string, xpub: string}
 type WZWallet = {
-  receive: HDWallet,
-  change: HDWallet,
-  defi: HDWallet,
-  balance: bigint
+  receive?: HDWallet | undefined,
+  change?: HDWallet | undefined,
+  defi?: HDWallet | undefined,
+  balance?: bigint | undefined
 }
 
 const wzDappMgr = ref();
@@ -63,56 +64,80 @@ export const useWizardConnect = () => {
     });
   }
 
-  onMounted(async () => {
+  const wzCreateWalletObject = async (wzSession: StoredSession) => {
+    if (!wzSession.paths || wzSession.paths?.length === 0) return {}
+    const receiveXPub = wzSession.paths.find((p: WZWalletPath) => p.name === 'receive')?.xpub
+    const changeXPub = wzSession.paths.find((p: WZWalletPath) => p.name === 'change')?.xpub
+    const defiXPub = wzSession.paths.find((p: WZWalletPath) => p.name === 'defi')?.xpub
+
+    const HDWalletClass = await getHDWalletClass()
+    const receiveWallet = receiveXPub && await HDWalletClass.fromXPub(receiveXPub)
+    const changeWallet = changeXPub && await HDWalletClass.fromXPub(changeXPub)
+    const defiWallet = defiXPub && await HDWalletClass.fromXPub(defiXPub)
+    return {
+      receive: receiveWallet,
+      change: changeWallet,
+      defi: defiWallet,
+      balance: 0n
+    } as WZWallet
+  }
+
+  const wzWalletGetUtxos = async (wzWallet: WZWallet) => {
+    const utxoRequests: { name: string, req: Promise<Utxo[]>}[] = []
+
+    if (wzWallet.receive) utxoRequests.push({ name: 'receive', req: wzWallet.receive.getUtxos() })
+    if (wzWallet.change) utxoRequests.push({ name: 'change', req: wzWallet.change.getUtxos() })
+    if (wzWallet.defi) utxoRequests.push({ name: 'defi', req: wzWallet.defi.getUtxos() })
+
+    const utxoPromiseResults = await Promise.allSettled([...utxoRequests.map((r) => r.req)])
+
+    let utxos: Utxo[] = []
+    
+    for (const i in utxoRequests) {
+      if(utxoPromiseResults[i]?.status === 'rejected') continue
+      utxos = utxos.concat(
+        (utxoPromiseResults[i] as PromiseFulfilledResult<Utxo[]>).value.map((u: Utxo) => ({ ...u, pathName: utxoRequests[i]!.name }))
+      )
+    }
+    return utxos
+  }
+
+  const wzWalletGetBalance = async (wzWallet: WZWallet) => {
+    const utxos = await wzWalletGetUtxos(wzWallet)
+    const utxoMap = new Map()
+    const balance = utxos!.reduce((acc, next) => {
+      if (next.token) return acc
+      if(utxoMap.has(`${next.txid}:${next.vout}`)) return acc
+      utxoMap.set(`${next.txid}:${next.vout}`, next)
+      acc = acc + next.satoshis
+      return acc
+    }, 0n)
+
+    return balance
+  }
+
+  const wzInitWallet = async () => {
     const { loadSession } = await import('@wizardconnect/dapp')
+    wzSession.value = loadSession()
+    if (wzSession.value) {
+      wzWallet.value = await wzCreateWalletObject(wzSession.value)
+    }
+    if (wzWallet.value) {
+      wzWallet.value.balance = await wzWalletGetBalance(wzWallet.value)
+    }
+  }
+
+  onMounted(async () => {
+    
     if (!wzDappMgr.value) {
       wzDappMgr.value = await getDappMgr(
         process.env.APP_NAME as string,
         process.env.APP_ICON_URL as string,
       );
-      wzSession.value = loadSession()
-
-      const receiveXPub = wzSession.value.paths.find((p: WZWalletPath) => p.name === 'receive').xpub
-      const changeXPub = wzSession.value.paths.find((p: WZWalletPath) => p.name === 'change').xpub
-      const defiXPub = wzSession.value.paths.find((p: WZWalletPath) => p.name === 'defi').xpub
-
-      const HDWalletClass = await getHDWalletClass()
-      const receiveWallet = await HDWalletClass.fromXPub(receiveXPub)
-      const changeWallet = await HDWalletClass.fromXPub(changeXPub)
-      const defiWallet = await HDWalletClass.fromXPub(defiXPub)
-      wzWallet.value = {
-        receive: receiveWallet,
-        change: changeWallet,
-        defi: defiWallet,
-        balance: 0n
-      }
-
-      const utxoRequests = [
-        { name: 'receive', req: receiveWallet.getUtxos() },
-        { name: 'change', req: changeWallet.getUtxos() },
-        { name: 'defi', req: defiWallet.getUtxos() }
-      ]
-      
-      const utxoPromiseResults = await Promise.allSettled([...utxoRequests.map((r) => r.req)])
-
-      let utxos: Utxo[] = []
-      
-      for (const i in utxoRequests) {
-        if(utxoPromiseResults[i].status === 'rejected') continue
-        utxos = utxos.concat(
-          (utxoPromiseResults[i] as PromiseFulfilledResult<Utxo[]>).value.map((u: Utxo) => ({ ...u, pathName: utxoRequests[i].name }))
-        )
-      }
-      const utxoMap = new Map()
-      const balance = utxos.reduce((acc, next) => {
-        if (next.token) return acc
-        if(utxoMap.has(`${next.txid}:${next.vout}`)) return acc
-        utxoMap.set(`${next.txid}:${next.vout}`, next)
-        acc = acc + next.satoshis
-        return acc
-      }, 0n)
-      wzWallet.value.balance = balance
     }
+    
+    await wzInitWallet()
+    
   });
 
   return {
