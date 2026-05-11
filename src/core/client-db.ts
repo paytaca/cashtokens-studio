2fe71d690193ea25044bce90deeef95707546c1b
hantyrram <rongrammer@hotmail.com>
2026-05-11 16:37:43 +0800

    store indexed identity snapshot to db for faster load time

--- src/core/client-db.ts
+++ src/core/client-db.ts
@@ -1,6 +1,6 @@
 // db.js
 import { Dexie, EntityTable} from 'dexie';
-import { Registry } from './bcmr/bcmr-v2.schema';
+import { Registry, TokenCategory, IdentitySnapshot} from './bcmr/bcmr-v2.schema';
 
 export type RegistryRecord = {
     id: number,
@@ -12,12 +12,29 @@ export type RegistryRecord = {
     registry: Registry
 }
 
+export type TokenCategoryRecord = {
+  id: number,
+  authbase: string,     // for indexing
+  category: string     // for indexing
+  tokenCategory: TokenCategory & Record<string, any>
+}
+
+export type IdentitySnapshotRecord = {
+  id: number,
+  authbase: string,     // for indexing
+  category: string     // for indexing
+  identitySnapshot: IdentitySnapshot & Record<string, any>
+}
+
 export const db = new Dexie('CashtokensStudioDB') as Dexie & {
+    // Use registry for recent active registries only,
     registry: EntityTable<RegistryRecord, 'id'> // primary key "id" (for the typings only)>;
+    identitySnapshot: EntityTable<IdentitySnapshotRecord, 'id'>
 }
 
 db.version(1).stores({
   registry: '++id, authbase, category, contentHash, latestRevision', // Primary key and indexed props
+  identitySnapshot: '++id, authbase, category'
 });
 
 
