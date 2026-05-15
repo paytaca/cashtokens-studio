70119bf526340f6ad6202165288e5aa8c7dde3f4
hantyrram <rongrammer@hotmail.com>
2026-05-15 20:15:09 +0800

    update i18n imports

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -11,6 +11,13 @@ export default {
         preparingTx: 'Preparing transaction, please wait...',
         waitingForSignature: 'Waiting for signature, please check your wallet...',
         broadcastingTx: 'Broadcasting transaction, please wait...',
+        authkeyTokenIdCandidateHint: 'This will become the token id of your Authkey NFT',
+        authkeyTokenIdCandidateNotFoundHint: 'No available token id candidate',
+        authkeyTokenIdCandidateExplainer: `<p>Creating a new 'Authkey' requires a 'genesis input'. A valid genesis input is
+            just an unspent BCH which is the 1st output of a previous transaction.</p><p> The txid of this
+            genesis input becomes the future token id of the token you create, in this case when you
+            create an Authkey Non-Fungible Token.</p>`,
+        whatsThis: `What's this?`
     },
     error: {
         genesisInputCreation: 'Error creating genesis input.',
