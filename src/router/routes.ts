3ded84c45e9f6850a5dce4a0c8a0d8b64aad3aa3
hantyrram <rongrammer@hotmail.com>
2026-05-15 20:14:37 +0800

    update routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,94 +6,155 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
+  // {
+  //   path: '/issuer',
+  //   component: () => import('layouts/MainLayout.vue'),
+  //   children: [
+  //     {
+  //       name: 'create-authkey',
+  //       path: 'tokens/create/authkey',
+  //       component: () => import('pages/issuer/CreateAuthKey.vue'),
+  //     },
+  //     // {
+  //     //   name: 'create-token',
+  //     //   path: 'tokens/create',
+  //     //   component: () => import('pages/issuer/TokenGenesis.vue'),
+  //     // },
+  //     {
+  //       name: 'import-auth-utxo',
+  //       path: 'tokens/import-auth-utxo',
+  //       component: () => import('pages/issuer/ImportAuthUtxo.vue'),
+  //     },
+  //     {
+  //       name: 'mint-nft',
+  //       path: 'tokens/mint-nft',
+  //       component: () => import('pages/issuer/MintNft.vue'),
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: '/issuer/manage',
+  //   component: () => import('layouts/MainLayout.vue'),
+  //   children: [
+  //     {
+  //       name: 'ft-reserves',
+  //       path: 'ft-reserves',
+  //       component: () => import('pages/issuer/manage/FtReserves.vue'),
+  //     },
+  //     {
+  //       name: 'nft-reserves',
+  //       path: 'nft-reserves',
+  //       component: () => import('pages/issuer/manage/NftReserves.vue'),
+  //     },
+  //     {
+  //       name: 'registries',
+  //       path: 'registries',
+  //       component: () => import('pages/issuer/manage/Registries.vue'),
+  //     },
+  //     {
+  //       name: 'authkeys',
+  //       path: 'authkeys',
+  //       component: () => import('pages/issuer/manage/AuthKeys.vue'),
+  //     },
+  //     {
+  //       name: 'authguards',
+  //       path: 'authguards',
+  //       component: () => import('pages/issuer/manage/AuthGuards.vue'),
+  //     },
+  //     {
+  //       path: 'token/:identifier',
+  //       component: () => import('pages/TokenMetadata.vue'),
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: '/account/balance',
+  //   component: () => import('layouts/MainLayout.vue'),
+  //   children: [
+  //     {
+  //       name: 'my-fts',
+  //       path: 'fungibletokens',
+  //       component: () => import('pages/account/balances/FungibleTokens.vue'),
+  //     },
+  //     {
+  //       name: 'my-nfts',
+  //       path: 'collectibles',
+  //       component: () => import('pages/account/balances/NonFungibleTokens.vue'),
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: '/account',
+  //   component: () => import('layouts/MainLayout.vue'),
+  //   children: [
+  //     {
+  //       name: 'recent-transactions',
+  //       path: 'recent-transactions',
+  //       component: () => import('pages/account/TransactionLogs.vue'),
+  //     },
+  //   ],
+  // },
   {
     path: '/issuer',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       {
-        name: 'create-authkey',
-        path: 'tokens/create/authkey',
-        component: () => import('pages/issuer/CreateAuthKey.vue'),
+        name: 'fungible-reserves',
+        path: 'fungible-reserves',
+        component: () => import('pages/issuer/manage/FungibleReservesPage.vue'),
       },
-      {
-        name: 'create-token',
-        path: 'tokens/create',
-        component: () => import('pages/issuer/TokenGenesis.vue'),
-      },
-      {
-        name: 'import-auth-utxo',
-        path: 'tokens/import-auth-utxo',
-        component: () => import('pages/issuer/ImportAuthUtxo.vue'),
-      },
-      {
-        name: 'mint-nft',
-        path: 'tokens/mint-nft',
-        component: () => import('pages/issuer/MintNft.vue'),
-      },
-    ],
+    ]
   },
   {
-    path: '/issuer/manage',
+    path: '/authguard',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       {
-        name: 'ft-reserves',
-        path: 'ft-reserves',
-        component: () => import('pages/issuer/manage/FtReserves.vue'),
-      },
-      {
-        name: 'nft-reserves',
-        path: 'nft-reserves',
-        component: () => import('pages/issuer/manage/NftReserves.vue'),
-      },
-      {
-        name: 'registries',
-        path: 'registries',
-        component: () => import('pages/issuer/manage/Registries.vue'),
+        name: 'create-authkey',
+        path: 'authkeys/create',
+        component: () => import('pages/authguard/CreateAuthkeyPage.vue'),
       },
       {
         name: 'authkeys',
         path: 'authkeys',
-        component: () => import('pages/issuer/manage/AuthKeys.vue'),
+        component: () => import('pages/authguard/AuthkeysPage.vue'),
       },
-      {
-        name: 'authguards',
-        path: 'authguards',
-        component: () => import('pages/issuer/manage/AuthGuards.vue'),
-      },
-      {
-        path: 'token/:identifier',
-        component: () => import('pages/TokenMetadata.vue'),
-      },
-    ],
+    ]
   },
+  
   {
-    path: '/account/balance',
+    path: '/token',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       {
-        name: 'my-fts',
-        path: 'fungibletokens',
-        component: () => import('pages/account/balances/FungibleTokens.vue'),
+        name: 'create-token',
+        path: 'create',
+        component: () => import('pages/token/CreateTokenPage.vue'),
       },
-      {
-        name: 'my-nfts',
-        path: 'collectibles',
-        component: () => import('pages/account/balances/NonFungibleTokens.vue'),
-      },
-    ],
+    ]
   },
   {
-    path: '/account',
+    path: '/wizard-connect',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       {
-        name: 'recent-transactions',
-        path: 'recent-transactions',
-        component: () => import('pages/account/TransactionLogs.vue'),
+        name: 'requests',
+        path: 'requests',
+        component: () => import('pages/wizard-connect/RequestsPage.vue'),
       },
-    ],
+    ]
   },
+  // {
+  //   path: '/authguard',
+  //   component: () => import('layouts/MainLayout.vue'),
+  //   children: [
+  //     {
+  //       name: 'create-authkey',
+  //       path: 'authkey/create',
+  //       component: () => import('pages/authguard/CreateAuthKeyPage.vue'),
+  //     },
+  //   ]
+  // },
   // Always leave this as last one,
   // but you can also remove it
   {
