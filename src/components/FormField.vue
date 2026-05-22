9ccdb12a4cdb25efe5cc525db15ee77075e4d840
hantyrram <rongrammer@hotmail.com>
2026-05-22 16:52:26 +0800

    component formfield component

---
+++ src/components/FormField.vue
@@ -0,0 +1,8 @@
+<template>
+  <div :class="className" class="flex column q-gutter-y-sm q-my-sm">
+    <slot></slot>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps<{ className?: string }>()
+</script>
