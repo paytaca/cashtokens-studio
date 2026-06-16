c012b53010c168a0b38dd49aba552e01213b5fd1
hantyrram <rongrammer@hotmail.com>
2026-06-16 16:46:47 +0800

    update return type, attach flag indicating if utxo is authhead

--- src/core/authguard/get-locked-utxos.ts
+++ src/core/authguard/get-locked-utxos.ts
@@ -1,19 +1,18 @@
 import { createAuthguardContract } from "./create-authguard-contract"
-import { UtxoWithPath } from "../types"
+import type { UtxoWithPath, AuthheadUtxo, DecoratedUtxo} from "../types"
 
-export type UtxoWithAuthKey = UtxoWithPath & { authkey?: UtxoWithPath }
-
-export async function getLockedAuthheadUtxos(authkeys: UtxoWithPath[]): Promise<UtxoWithAuthKey[]> {
-    const lockedUtxos: UtxoWithAuthKey[] = []
+export async function getLockedAuthheadUtxos(authkeys: UtxoWithPath[]): Promise<DecoratedUtxo[]> {
+    const lockedUtxos: DecoratedUtxo[] = []
     for (const authkey of authkeys) {
         const authguard = createAuthguardContract({
             authkeyTokenId: authkey.token!.category,
             network: import.meta.env.VITE_BCH_NETWORK
         })
-        const utxos: UtxoWithAuthKey[] = await authguard.getUtxos() as UtxoWithAuthKey[]
+        const utxos: DecoratedUtxo[] = await authguard.getUtxos() as DecoratedUtxo[]
         utxos.forEach((u) => {
             u.address = authguard.address
             u.authkey = authkey
+            u.isAuthhead = true
         })
         lockedUtxos.push(...utxos)
     }
