2a2dbedccb890c02c728f30bb084e6521d53cd3a
hantyrram <rongrammer@hotmail.com>
2026-07-07 13:10:17 +0800

    route to main page on disconnect

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,31 @@
   <router-view />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { watch } from 'vue';
+import { useWizardConnectWallet } from './composables/useWizardConnectWallet';
+import { WizardConnectExternalWallet } from './core/wallet';
+import { PathXpub } from '@wizardconnect/core';
+import { WizardConnectState } from 'wizardconnect-vue';
+import { useRouter } from 'vue-router';
+const { manager, wallet, walletLasySync, state } = useWizardConnectWallet()
+const router = useRouter()
 
-export default defineComponent({
-  name: 'App'
+watch(() => manager.value, async (newV, oldV) => {
+  if (!wallet.value?.ready && !oldV && newV?.getSessionPaths()) {
+    wallet.value = new WizardConnectExternalWallet({
+      network: import.meta.env.VITE_BCH_NETWORK
+    })
+    await wallet.value!.initWallet({ paths: manager.value!.getSessionPaths() as PathXpub[] })
+    await wallet.value.getBalance({ sync: true })
+    walletLasySync.value = Date.now()
+  }
 })
+
+watch(() => state.value, (v: WizardConnectState) => {
+  if (v === 'disconnected') {
+    router.push('/')
+  }
+})
+
 </script>
