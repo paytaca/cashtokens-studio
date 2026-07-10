5c51d57251fe7fd871ac780cb949058055c953f4
hantyrram <rongrammer@hotmail.com>
2026-07-10 14:42:35 +0800

    add callback param for broadcast progress

--- src/services/transaction.ts
+++ src/services/transaction.ts
@@ -3,23 +3,31 @@ import { safeAsync } from "src/core/utils";
 import { broadcastTransaction as watchtowerBroadcast } from "./watchtower";
 import { NetworkType, BaseWallet } from "mainnet-js-v3";
 
-export async function broadcastTransaction(transactionHex: string, network?: 'chipnet' | 'mainnet' | 'testnet'): Promise<any> {
+export type BroadcastTransactionParams = {
+    transactionHex: string,
+    network?: 'chipnet' | 'mainnet' | 'testnet', 
+    onProgress?: (progress: string) => void
+}
 
-    const [watchtowerBroadcastError, watchtowerBroadcastResult] = await safeAsync(watchtowerBroadcast(transactionHex))
+export async function broadcastTransaction(params: BroadcastTransactionParams ): Promise<any> {
+
+    const [watchtowerBroadcastError, watchtowerBroadcastResult] = await safeAsync(watchtowerBroadcast(params.transactionHex))
 
     if (!watchtowerBroadcastError && isBroadcastSuccess(watchtowerBroadcastResult)) {
         return [watchtowerBroadcastError, watchtowerBroadcastResult.txid]
     }
 
+    params.onProgress?.('Primary broadcast provider unreachable. Trying alternative route. Please wait...')
+
     let n = NetworkType.Mainnet
-    if (network && network !== NetworkType.Mainnet) {
-        n = network && NetworkType.Testnet
+    if (params.network && params.network !== NetworkType.Mainnet) {
+        n = params.network && NetworkType.Testnet
     }
 
     const baseWallet = new BaseWallet(n)
 
     const [electrumBroadcastError, electrumBroadcastResult] = 
-        await safeAsync(baseWallet.provider.sendRawTransaction(transactionHex, true))
+        await safeAsync(baseWallet.provider.sendRawTransaction(params.transactionHex, true))
     
     return [electrumBroadcastError, electrumBroadcastResult]
   }
