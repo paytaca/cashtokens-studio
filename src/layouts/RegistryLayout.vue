8f7854a7240cfa9096181fb4ecded58407aea714
hantyrram <rongrammer@hotmail.com>
2026-07-11 11:30:10 +0800

    refactor(RegistryLayout): simplify toggleWriteMode, fix Token ID overflow, clean up markup

--- src/layouts/RegistryLayout.vue
+++ src/layouts/RegistryLayout.vue
@@ -3,53 +3,44 @@
         <div class="row justify-center">
             <div class="col-xs-12 col-sm-10 col-md-8">
                 <div class="q-mb-md q-px-sm">
-                    <q-btn flat dense icon="arrow_back" label="Back" color="grey-4"
-                        @click="router.push({ path: '/dashboard/#/created' })" />
+                    <q-btn flat dense icon="arrow_back" label="Back" color="grey-4" @click="router.back()" />
                 </div>
                 <q-card flat class="bg-dark q-pa-lg rounded-borders">
-
-                    <div class="flex justify-between items-start">
-                        <div class="row items-center q-gutter-x-md q-mb-lg reverse-wrap">
-                            <div class="flex no-wrap items-center q-gutter-x-md ">
-                                <q-avatar :size="$q.screen.lt.sm ? '5rem' : '8rem'"
-                                    class="bg-grey-9 border-radius-8 shadow-1">
-                                    <q-img v-if="activeIdentitySnapshot?.uris?.icon"
-                                        :src="ipfsToGatewayUrl(activeIdentitySnapshot.uris.icon)!" fit="cover" />
-                                    <q-icon v-else name="token" color="primary" size="32px" />
-                                </q-avatar>
-                                <div class="q-pa-sm q-gutter-y-sm">
-                                    <div class="flex items-center q-mt-xs token-symbol">
-                                        {{ activeIdentitySnapshot?.token?.symbol || "Unknown" }}
-                                    </div>
-                                    <div class="text-body2 text-mono text-grey-2 word-break-all ellipsis">
-                                        Token ID: {{ $q.screen.lt.md ?
-                                            shortenTokenId(activeIdentitySnapshot?.token!.category as
-                                                string) : activeIdentitySnapshot?.token!.category }}
-                                        <CopyText v-if="activeIdentitySnapshot?.token?.category"
-                                            :text="activeIdentitySnapshot?.token?.category" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    <div class="row justify-end">
                         <q-btn v-if="activeAuthhead" icon="mdi-text-box-edit" :label="$q.screen.gt.xs ? 'Edit' : ''"
                             dense flat color="secondary" @click="toggleWriteMode">
                         </q-btn>
                     </div>
-
-                    <!-- Added dark background helper for contrast with white text -->
-                    <nav class="inline-nav bg-dark q-py-sm">
+                    <div class="flex justify-between items-start">
+                        <div class="flex no-wrap items-center q-gutter-x-md">
+                            <q-avatar :size="$q.screen.lt.sm ? '5rem' : '8rem'"
+                                class="bg-grey-9 border-radius-8 shadow-1">
+                                <q-img v-if="activeIdentitySnapshot?.uris?.icon"
+                                    :src="ipfsToGatewayUrl(activeIdentitySnapshot.uris.icon)!" fit="cover" />
+                                <q-icon v-else name="token" color="primary" size="32px" />
+                            </q-avatar>
+                            <div class="q-pa-sm q-gutter-y-sm" style="min-width: 0">
+                                <div class="flex items-center q-mt-xs token-symbol">
+                                    {{ activeIdentitySnapshot?.token?.symbol || "Unknown" }}
+                                </div>
+                                <div class="text-mono text-grey-2 ellipsis">
+                                    Token ID: {{ $q.screen.lt.lg ?
+                                        shortenTokenId(activeIdentitySnapshot?.token!.category as
+                                            string) : activeIdentitySnapshot?.token!.category }}
+                                    <CopyText v-if="activeIdentitySnapshot?.token?.category"
+                                        :text="activeIdentitySnapshot?.token?.category" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <nav class="inline-nav bg-dark q-py-lg">
                         <div class="row no-wrap items-center justify-start q-gutter-x-sm">
-
-                            <!-- Navigation Link Tabs -->
                             <q-item v-for="link in navLinks" :key="link.title" clickable v-ripple :to="link.to" exact
                                 active-class="inline-item-active"
                                 class="inline-item q-px-md q-py-sm rounded-borders text-no-wrap">
-                                <!-- Icon Section -->
                                 <q-item-section v-if="link.icon" avatar class="inline-icon-section q-mr-sm">
                                     <q-icon :name="link.icon" size="20px" class="inline-icon" />
                                 </q-item-section>
-
-                                <!-- Label Section -->
                                 <q-item-section class="inline-text-section">
                                     <span class="nav-text-wrapper">
                                         <q-item-label class="text-weight-medium text-body2">
@@ -84,35 +75,38 @@ const { activeAuthhead } = useAuthguardStore()
 const route = useRoute()
 
 const router = useRouter()
+const mode = ref<'edit' | 'view'>('view')
 
-const navLinks = ref([
-    { title: 'Token Identity', caption: 'Overview & analytics', icon: 'dashboard', to: { name: 'view-identity-snapshot', params: route.params, query: route.query } },
-    { title: 'NFTs', caption: 'Manage your tasks', icon: 'assignment', to: { name: 'view-identity-snapshot-nfts', params: route.params, query: route.query } },
-    { title: 'Registry', caption: 'Collaborate with members', icon: 'people', to: { name: 'view-registry', params: route.params, query: route.query } },
-])
+const navModeLinks = {
+    view: [
+        { title: 'Token Identity', caption: 'Overview & analytics', icon: 'dashboard', to: { name: 'view-identity-snapshot', params: route.params, query: route.query } },
+        { title: 'NFTs', caption: 'Manage your tasks', icon: 'assignment', to: { name: 'view-identity-snapshot-nfts', params: route.params, query: route.query } },
+        { title: 'Registry', caption: 'Collaborate with members', icon: 'people', to: { name: 'view-registry', params: route.params, query: route.query } },
+    ],
+    edit: [
+        { title: 'Token Identity', caption: 'Overview & analytics', icon: 'dashboard', to: { name: 'edit-identity-snapshot', params: route.params, query: route.query } },
+        { title: 'NFTs', caption: 'Manage your tasks', icon: 'assignment', to: { name: 'edit-identity-snapshot-nfts', params: route.params, query: route.query } },
+        { title: 'Registry', caption: 'Collaborate with members', icon: 'people', to: { name: 'edit-registry', params: route.params, query: route.query } },
+    ]
+}
+
+const navLinks = ref(navModeLinks.view)
 
 const toggleWriteMode = () => {
-    console.log('route query', typeof (route.query.write))
-    const current = !route.query.write || route.query.write === '0' ? 'view' : 'edit'
-    const newMode = current === 'view' ? 'edit' : 'view'
+    const paths = route.path.split('/')
+    const currentMode = paths[paths.length - 1]
+    mode.value = route.path.endsWith('view') ? 'edit' : 'view'
+    navLinks.value = navModeLinks[mode.value]
     router.push({
-        path: route.path.replace(current, newMode),
+        path: route.path.replace(currentMode as string, mode.value),
         query: {
             ...route.query,
-            write: route.query.write == '1' ? 0 : 1
         }
     })
 }
-
 </script>
 
-
-
 <style scoped>
-/* ==========================================================================
-   Inline Navigation Component Styles (White Text & Active Underline)
-   ========================================================================== */
-
 .inline-nav {
     background: transparent;
     width: 100%;
