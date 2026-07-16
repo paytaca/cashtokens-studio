b6aca59348f0108251d3a3d2e467c4a4ee467c4d
hantyrram <rongrammer@hotmail.com>
2026-07-16 16:08:18 +0800

    auto visit dashboard if wallet changed

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,32 @@
 </template>
 
 <script setup lang="ts">
-import { watch, provide } from 'vue';
+import { watch, provide, ref } from 'vue';
 import { useWizardConnectWallet } from './composables/useWizardConnectWallet';
 import { useRoute, useRouter } from 'vue-router';
 import { useAuthguardStore } from './stores/authguard';
 import { UtxoWithAuthKey, UtxoWithPath } from './core/types';
-
+import { AuthheadId } from './core/authguard';
+import { useRegistryStore } from './stores/registry';
+import { storeToRefs } from 'pinia';
+const { setActiveIdentitySnapshot } = useRegistryStore()
 const wizardConnectWallet = useWizardConnectWallet()
-const { activeAuthhead, setActiveAuthhead, loadAuthhead } = useAuthguardStore()
+
+const authguardStore = useAuthguardStore()
+const { setActiveAuthhead, loadAuthhead } = authguardStore
+const { activeAuthhead } = storeToRefs(authguardStore)
 
 provide('wizardConnectWallet', wizardConnectWallet)
 
 const route = useRoute()
 const router = useRouter()
+const dashboardVisited = ref<boolean>(false)
 
 router.beforeEach(async (to) => {
-  if (to.path === '/authhead' && to.query.authkey && !activeAuthhead) {
+  console.log('@route to', to, activeAuthhead.value)
+  if (to.path.startsWith('/registry') && activeAuthhead.value) return
+
+  if (to.path.startsWith('/registry') && to.query.authkey && !activeAuthhead.value) {
     const [txid, vout] = (to.query.authkey as string).split(':')
     if (!txid || !vout) {
       return {
@@ -36,15 +46,22 @@ router.beforeEach(async (to) => {
     }
 
     if (authkey) {
-      const authhead = await loadAuthhead(authkey as UtxoWithPath)
+      const authhead = await loadAuthhead({ authkey, authheadId: route.query?.authhead as AuthheadId })
+      if (!authhead) {
+        return {
+          path: '/dashboard'
+        }
+      }
       setActiveAuthhead(authhead as UtxoWithAuthKey)
+      setActiveIdentitySnapshot(authhead.identitySnapshot)
     }
   }
 
 })
 
 watch(() => wizardConnectWallet.walletIsReady.value, (isReady) => {
-  if (isReady && route.path === '/loading') {
+  console.log('wallet is ready', isReady)
+  if (isReady && (route.path === '/loading' || !dashboardVisited.value)) {
     const path = route.redirectedFrom?.fullPath || '/dashboard'
     router.replace({
       path: path,
